12d2d87ea3735fce818f6a68cd8a3fbc000e5082
luoxinnan <luoxinnan1@gmail.com>
2024-06-04 14:24:40 +0200

    Creates form in AddDiplomaForm

---
+++ frontend/src/components/AddDiplomaForm.tsx
@@ -0,0 +1,26 @@
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form"
+
+
+export default function AddDeplomaForm(){
+    const {register, handleSubmit} = useForm();
+
+    function submitAndMakePDF(data: FieldValues){
+        console.log(data);
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit(data => {submitAndMakePDF(data)})}>
+            <select {...register("classname")}>
+                <option value="dotnet-winter-2024">Dotnet winter 2024</option>
+                <option value="java-autumn-2023">Java Autumn 2023</option>
+                <option value="js-spring-2024">JavaScript Spring 2024</option>
+            </select>
+            <input {...register("date-of-bootcamp")} type="date" />
+            <textarea {...register("names")} />
+            <button>Choose</button>
+            <input type="submit" value="Generate"/>
+        </form>
+        </> 
+    )
+}
