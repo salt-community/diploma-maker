fc0521b8d5a383d2065dc7af77a35266d777a7a4
Zoldier777 <Zzer0ph@gmail.com>
2024-06-04 13:21:14 +0200

    feat: disply pdf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import PDFfile from "./components/PDFfile.tsx";
+import { PDFViewer } from '@react-pdf/renderer';
+import './App.css'
 
 function App() {
-
   return (
-    <h1>hello</h1>
-  )
+    <div className="App">
+      <nav></nav>
+    
+      <section className="PDFView"><h1>Preview PDF</h1>
+      <PDFViewer width={500} height={750} showToolbar={false}>
+        <PDFfile />
+      </PDFViewer>
+      </section>
+      <section className="Input">
+        <h2>Form section</h2>   
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/PDFfile.tsx
+++ frontend/src/components/PDFfile.tsx
@@ -15,15 +15,16 @@ const styles = StyleSheet.create({
 });
 
 // Create Document Component
-const MyDocument = () => (
-  <Document>
+const PDFfile = () => (
+  <Document pageMode='fullScreen'>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
         <Text>Section #1</Text>
+        
       </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
+
     </Page>
   </Document>
 );
+
+export default PDFfile;
