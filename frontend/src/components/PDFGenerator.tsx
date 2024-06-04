9227a05d323f17b12d04d2b5f8a768fbbb1adb3e
luoxinnan <luoxinnan1@gmail.com>
2024-06-04 15:20:36 +0200

    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/components/PDFGenerator.tsx
+++ frontend/src/components/PDFGenerator.tsx
@@ -1,15 +1,19 @@
 import PDFfile from "../components/PDFfile.tsx";
 import { PDFViewer } from "@react-pdf/renderer";
 
-export default function PDFGenerator() {
+export default function PDFGenerator({names} : Props) {
   return (
     <>
       <section className="PDFView">
-        <h1>Preview PDF</h1>
+        <h1>Preview PDF {names} </h1>
         <PDFViewer width={500} height={750} showToolbar={false}>
           <PDFfile />
         </PDFViewer>
       </section>
     </>
   );
+}
+
+type Props = {
+  names : string[]
 }
