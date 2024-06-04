b8ea43ef1feee63b28ac8b257cf2fd8496e751f3
luoxinnan <luoxinnan1@gmail.com>
2024-06-04 13:49:34 +0200

    Move PDF generator to its own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import PDFfile from "./components/PDFfile.tsx";
-import { PDFViewer } from '@react-pdf/renderer';
 import './App.css'
+import {Routes, Route, } from "react-router-dom";
+import PDFGenerator from './components/PDFGenerator.tsx';
+
+
 
 function App() {
   return (
     <div className="App">
-      <nav></nav>
-    
-      <section className="PDFView"><h1>Preview PDF</h1>
-      <PDFViewer width={500} height={750} showToolbar={false}>
-        <PDFfile />
-      </PDFViewer>
-      </section>
-      <section className="Input">
-        <h2>Form section</h2>   
-      </section>
+      <Routes>
+        <Route path="/" element={<PDFGenerator/>} />
+      </Routes>
     </div>
   );
 }
