99b86b9967de4f311cd9038edd0b9b609e9ad0ac
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-06 09:36:14 +0200

    add: uiRefs, mode, PreviewWindow
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/DiplomaMaking.tsx
+++ frontend/src/pages/DiplomaMaking.tsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import AddDeplomaForm from "../components/AddDiplomaForm";
 import PDFGenerator from "../components/PDFGenerator";
 import { Template, checkTemplate } from "@pdfme/common";
 import { getTemplate } from "../templates/baseTemplate";
+import { Form, Viewer } from "@pdfme/ui";
+import {
+  getFontsData,
+  handleLoadTemplate,
+  generatePDF,
+  getPlugins,
+  isJsonString,
+} from "../util/helper";
 
 type Mode = "form" | "viewer";
 
@@ -24,15 +32,67 @@ const initTemplate = () => {
 export default function DiplimaMaking(){
     const [studentNames, setStudentNames] = useState<string[]>([]);
 
-    function UpdateStudentNames(names: string[]){
+    const uiRef = useRef<HTMLDivElement | null>(null);
+    const ui = useRef<Form | Viewer | null>(null);
+
+    const [mode, setMode] = useState<Mode>(
+      (localStorage.getItem("mode") as Mode) ?? "form"
+    );
+
+    const UpdateStudentNames = (names: string[]) => {
         setStudentNames(names);
     }
 
+    
+
     return (
         <>
-        <PDFGenerator names={studentNames}/>
-        <AddDeplomaForm updateStudentNames = {UpdateStudentNames}/>
-
+          {/* <PDFGenerator names={studentNames}/> */}
+          <div>
+            <header
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginRight: 120,
+              }}
+            >
+              <strong>Form, Viewer</strong>
+              <span style={{ margin: "0 1rem" }}>:</span>
+              <div>
+                <input
+                  type="radio"
+                  onChange={onChangeMode}
+                  id="form"
+                  value="form"
+                  checked={mode === "form"}
+                />
+                <label htmlFor="form">Form</label>
+                <input
+                  type="radio"
+                  onChange={onChangeMode}
+                  id="viewer"
+                  value="viewer"
+                  checked={mode === "viewer"}
+                />
+                <label htmlFor="viewer">Viewer</label>
+              </div>
+              <label style={{ width: 180 }}>
+                Load Template
+                <input
+                  type="file"
+                  accept="application/json"
+                  onChange={(e) => handleLoadTemplate(e, ui.current)}
+                />
+              </label>
+              <button onClick={() => generatePDF(ui.current)}>Generate PDF</button>
+            </header>
+            <div
+              ref={uiRef}
+              style={{ width: "100%", height: `calc(100vh - 68px)` }}
+            />
+          </div>
+          <AddDeplomaForm updateStudentNames = {UpdateStudentNames}/>
         </>
     )
 }
