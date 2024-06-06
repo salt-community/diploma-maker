dd9172090467d9fa11298e3bde7d1eb2b5fe3100
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-06 09:36:27 +0200

    add: Diplomamaking -> onChangeMode
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/DiplomaMaking.tsx
+++ frontend/src/pages/DiplomaMaking.tsx
@@ -43,7 +43,11 @@ export default function DiplimaMaking(){
         setStudentNames(names);
     }
 
-    
+    const onChangeMode = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value as Mode;
+      setMode(value);
+      localStorage.setItem("mode", value);
+    };
 
     return (
         <>
