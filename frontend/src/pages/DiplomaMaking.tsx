c10c498fd34e3bad1a1c22213c377d3c7c00e930
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-06 09:29:00 +0200

    add: DiplomaMaking -> initTemplate function
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/DiplomaMaking.tsx
+++ frontend/src/pages/DiplomaMaking.tsx
@@ -1,6 +1,25 @@
 import { useState } from "react";
 import AddDeplomaForm from "../components/AddDiplomaForm";
 import PDFGenerator from "../components/PDFGenerator";
+import { Template, checkTemplate } from "@pdfme/common";
+import { getTemplate } from "../templates/baseTemplate";
+
+type Mode = "form" | "viewer";
+
+const initTemplate = () => {
+  let template: Template = getTemplate();
+  try {
+    const templateString = localStorage.getItem("template");
+    const templateJson = templateString
+      ? JSON.parse(templateString)
+      : getTemplate();
+    checkTemplate(templateJson);
+    template = templateJson as Template;
+  } catch {
+    localStorage.removeItem("template");
+  }
+  return template;
+};
 
 export default function DiplimaMaking(){
     const [studentNames, setStudentNames] = useState<string[]>([]);
