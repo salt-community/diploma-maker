747f706e1abca7bb8ebe7aabb49f28c0b5fa55a7
luoxinnan <luoxinnan1@gmail.com>
2024-06-07 11:07:40 +0200

    Create types in frontend -
    Co-authored-by: william00771 <william.f.lindberg@hotmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/util/types.ts
+++ frontend/src/util/types.ts
@@ -1,1 +1,42 @@
 export type displayMode = "form" | "viewer";
+
+// diplomas
+
+export type DiplomaRequest = {
+    id: string;
+    graduationDate: Date;
+    studentName: string;
+    bootcampId: string;
+}
+
+export type DiplomaResponse = {
+    id: string;
+    graduationDate: Date;
+    studentName: string;
+    bootcamp: BootcampInDiploma;
+}
+
+export type DiplomaInBootcamp = {
+    id: string;
+    graduationDate: Date;
+    studentName: string;
+}
+
+// bootcamps
+
+export type BootcampInDiploma = {
+    id: string;
+    name: string;
+    startDate: Date;
+}
+export type BootcampRequest = {
+    id: string;
+    name: string;
+    startDate: Date;
+}
+export type BootcampResponse = {
+    id: string;
+    name: string;
+    startDate: Date;
+    diplomas: DiplomaInBootcamp;
+}
