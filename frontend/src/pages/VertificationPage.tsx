012570768a5b65635b3cd188e68af8854676ce57
luoxinnan <luoxinnan1@gmail.com>
2024-06-10 15:01:11 +0200

    Add basic tailwindcss to vertification -
    Co-authored-by: william00771 <william.f.lindberg@hotmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/VertificationPage.tsx
+++ frontend/src/pages/VertificationPage.tsx
@@ -20,9 +20,9 @@ export function VertificationPage(){
 
     return (
         <>
-        <h1>Congratulations!</h1>
-        <h2>This is to certify that {diploma!.studentName} successfully completed the traning on {new Date(diploma!.graduationDate).toLocaleDateString()} from the {diploma!.bootcamp.name}.</h2>
-        <h2>This diploma is valid and authentic.</h2>
+        <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">Congratulations!</h1>
+        <h2 className="mb-4 text-4xl text-center font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-3xl">This is to certify that {diploma!.studentName} successfully completed the traning on {new Date(diploma!.graduationDate).toLocaleDateString()} from the {diploma!.bootcamp.name}.</h2>
+        <h2 className="mb-4 text-4xl text-center font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-3xl">This diploma is valid and authentic.</h2>
         </>
     )
 }
