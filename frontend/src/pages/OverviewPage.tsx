faee5daa28512bebe66f981a5b1d7b8b074b5d6a
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-11 21:18:58 +0200

    refractor: OverviewPage -> add RemoveButton component
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/OverviewPage.tsx
+++ frontend/src/pages/OverviewPage.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import './OverviewPage.css';
 import { overviewPageItemsMockData } from '../Data/Mockdata';
-
-
+import RemoveButton from '../components/MenuItems/RemoveButton';
 
 export const OverviewPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
@@ -40,12 +39,6 @@ export const OverviewPage = () => {
                             <p className='overview-page__item--title'>{item.title}</p>
                             <img className='overview-page__item--bg' src={item.src} alt={item.title} />
                             <section className='overview-page__item--menu'>
-                                    <button className="btn remove-btn">
-                                        <span className="btn-title">Remove</span>
-                                        <svg className="btn-close-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
-                                        </svg>
-                                    </button>
                                     <button className="btn modify-btn">
                                         <span className="btn-title">Modify</span>
                                         <svg className="btn-close-icon" version="1.1" id="Uploaded to svgrepo.com" height="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" >
@@ -61,6 +54,7 @@ export const OverviewPage = () => {
                                             />
                                         </svg>
                                     </button>
+                                    <RemoveButton text='Remove'/>
                             </section>
                         </button>
                     ))}
