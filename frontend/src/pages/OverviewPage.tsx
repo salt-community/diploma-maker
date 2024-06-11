65ef529d711856eb763f80e28f36b54aa0944a18
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-11 14:07:42 +0200

    add: OverviewPage Bare Bones Styling
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/OverviewPage.tsx
+++ frontend/src/pages/OverviewPage.tsx
@@ -1,38 +1,73 @@
+import './OverviewPage.css';
+
 export const OverviewPage = () => {
     return(
-        <main>
-            <header>
-                <input type="text" placeholder="search" />
+        <main className="overview-page">
+            <header className="overview-page__header input-wrapper">
+                <input className="overview-page__search-input" type="text" placeholder="search" />
+                <svg className='input-icon' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
             </header>
-            <section>
-                <h2>Bootcamps</h2>
-                <div>
-                    <select>
+            <section className="overview-page__select">
+                <h2 className="overview-page__section-title">Bootcamps</h2>
+                <div className="overview-page__select-container select-wrapper">
+                    <select className="overview-page__select-box">
                         <option value=".Net Fullstack">Dotnet</option>
                         <option value="Java Fullstack">Java</option>
                         <option value="Javascript Fullstack">JavaScript</option>
                     </select>
                 </div>
             </section>
-            <section>
-                <div>
-                    <article className="item">Li lau</article>
-                    <article className="item">Luan karlsson</article>
-                    <article className="item">Willy Wonka</article>
-                    <article className="item">Tom Ryder</article>
-                    <article className="item">Page sage</article>
-                    <article className="item">Marco Pierre White</article>
-                    <article className="item">Junior JR</article>
-                    <article className="item">Tommy Lee</article>
+            <section className="overview-page__browse">
+                <div className="overview-page__item-container">
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Li lau</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Luan karlsson</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Willy Wonka</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Tom Ryder</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Page sage</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Marco Pierre White</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Junior JR</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
+                    <article className="overview-page__item">
+                        <p className='overview-page__item--title'>Tommy Lee</p>
+                        <img className='overview-page__item--bg' src="https://res.cloudinary.com/dlw9fdrql/image/upload/v1718105458/diploma_xmqcfi.jpg" alt="" />
+                    </article>
                 </div>
-                <footer>
-                    <div className="flex justify-center mt-4">
-                        <button>Previous</button>
-                        <span className="mx-4">
-                            Page 1 of 5
-                        </span>
-                        <button>Next</button>
-                    </div>
+                <footer className="overview-page__footer">
+                    <button className="overview-page__pagination-button iconbtn">
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 7L10 12L15 17" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                    <span className="overview-page__pagination-info">
+                        Page 1 of 5
+                    </span>
+                    <button className="overview-page__pagination-button iconbtn">
+                        <svg viewBox="0 0 24 24" style={{transform: 'rotate(180deg)'}} fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 7L10 12L15 17" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
                 </footer>
             </section>
         </main>
