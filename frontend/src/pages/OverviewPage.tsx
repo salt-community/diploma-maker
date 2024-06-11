fc5f55f1f5e64e58615c00915dad5281688dd8ac
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-11 12:20:12 +0200

    add: Bare Bones static HTML
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/OverviewPage.tsx
+++ frontend/src/pages/OverviewPage.tsx
@@ -1,7 +1,40 @@
 export const OverviewPage = () => {
     return(
         <main>
-            <h1>OverviewPage</h1>
+            <header>
+                <input type="text" placeholder="search" />
+            </header>
+            <section>
+                <h2>Bootcamps</h2>
+                <div>
+                    <select>
+                        <option value=".Net Fullstack">Dotnet</option>
+                        <option value="Java Fullstack">Java</option>
+                        <option value="Javascript Fullstack">JavaScript</option>
+                    </select>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <article className="item">Li lau</article>
+                    <article className="item">Luan karlsson</article>
+                    <article className="item">Willy Wonka</article>
+                    <article className="item">Tom Ryder</article>
+                    <article className="item">Page sage</article>
+                    <article className="item">Marco Pierre White</article>
+                    <article className="item">Junior JR</article>
+                    <article className="item">Tommy Lee</article>
+                </div>
+                <footer>
+                    <div className="flex justify-center mt-4">
+                        <button>Previous</button>
+                        <span className="mx-4">
+                            Page 1 of 5
+                        </span>
+                        <button>Next</button>
+                    </div>
+                </footer>
+            </section>
         </main>
     )
 }
