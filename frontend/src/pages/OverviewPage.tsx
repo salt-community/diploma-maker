8df96af3f9dfbc92bbbe0a83a7060b308e73ae6b
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-11 16:11:07 +0200

    add: OverviewPage -> Remove, Modify btn
    Co-authored-by: luoxinnan <luoxinnan1@gmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/pages/OverviewPage.tsx
+++ frontend/src/pages/OverviewPage.tsx
@@ -39,6 +39,29 @@ export const OverviewPage = () => {
                         <button key={index} className='listmodule__item'>
                             <p className='overview-page__item--title'>{item.title}</p>
                             <img className='overview-page__item--bg' src={item.src} alt={item.title} />
+                            <section className='overview-page__item--menu'>
+                                    <button className="btn remove-btn">
+                                        <span className="btn-title">Remove</span>
+                                        <svg className="btn-close-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
+                                        </svg>
+                                    </button>
+                                    <button className="btn modify-btn">
+                                        <span className="btn-title">Modify</span>
+                                        <svg className="btn-close-icon" version="1.1" id="Uploaded to svgrepo.com" height="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" >
+                                            <path d="M27.5,14h-2.731c-0.228-1.003-0.624-1.94-1.156-2.785l1.933-1.933c0.195-0.195,0.195-0.512,0-0.707
+                                                l-2.121-2.121c-0.195-0.195-0.512-0.195-0.707,0l-1.933,1.933C19.94,7.855,19.003,7.459,18,7.231V4.5C18,4.224,17.776,4,17.5,4h-3
+                                                C14.224,4,14,4.224,14,4.5v2.731c-1.003,0.228-1.94,0.624-2.785,1.156L9.282,6.454c-0.195-0.195-0.512-0.195-0.707,0L6.454,8.575
+                                                c-0.195,0.195-0.195,0.512,0,0.707l1.933,1.933C7.855,12.06,7.459,12.997,7.231,14H4.5C4.224,14,4,14.224,4,14.5v3
+                                                C4,17.776,4.224,18,4.5,18h2.731c0.228,1.003,0.624,1.94,1.156,2.785l-1.933,1.933c-0.195,0.195-0.195,0.512,0,0.707l2.121,2.121
+                                                c0.195,0.195,0.512,0.195,0.707,0l1.933-1.933c0.845,0.532,1.782,0.928,2.785,1.156V27.5c0,0.276,0.224,0.5,0.5,0.5h3
+                                                c0.276,0,0.5-0.224,0.5-0.5v-2.731c1.003-0.228,1.94-0.624,2.785-1.156l1.933,1.933c0.195,0.195,0.512,0.195,0.707,0l2.121-2.121
+                                                c0.195-0.195,0.195-0.512,0-0.707l-1.933-1.933c0.532-0.845,0.928-1.782,1.156-2.785H27.5c0.276,0,0.5-0.224,0.5-0.5v-3
+                                                C28,14.224,27.776,14,27.5,14z M16,21c-2.761,0-5-2.239-5-5s2.239-5,5-5s5,2.239,5,5S18.761,21,16,21z"
+                                            />
+                                        </svg>
+                                    </button>
+                            </section>
                         </button>
                     ))}
                 </div>
