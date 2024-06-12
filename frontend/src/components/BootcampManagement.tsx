ee85232f91a6f3c8e42ef8ab1ad64d88c2e5c9f1
luoxinnan <luoxinnan1@gmail.com>
2024-06-12 10:11:02 +0200

    Move dates in form to BootcampManagement -
    Co-authored-by: william00771 <william.f.lindberg@hotmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/src/components/BootcampManagement.tsx
+++ frontend/src/components/BootcampManagement.tsx
@@ -1,12 +1,14 @@
+import { FieldValues, UseFormRegister, useForm } from "react-hook-form";
 import { BootcampResponse } from "../util/types"
 import { EditText, EditTextarea } from 'react-edit-text';
 
 type Props = {
     bootcamps: BootcampResponse[];
     deleteBootcamp: (i: number) => Promise<void>;
+    register: UseFormRegister<FieldValues>;
 }
 
-export default function BootcampManagement({ bootcamps, deleteBootcamp }: Props) {
+export default function BootcampManagement({ bootcamps, deleteBootcamp}: Props) {
 
     return (
         <div className="block font-medium text-gray-700">
@@ -24,6 +26,33 @@ export default function BootcampManagement({ bootcamps, deleteBootcamp }: Props)
             <EditText placeholder="New bootcamp name" />
             <p></p>
             <button type="button" className="text-green-500">Add</button>
+
+              
+        <div className="dateofbootcamp">
+          <label htmlFor="datebootcamp" className="block text-sm font-medium text-gray-700">
+            Date of Bootcamp
+          </label>
+          <input
+            id="datebootcamp"
+            // {...register("datebootcamp")}
+            type="date"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            defaultValue={"2024-01-01"}
+          />
+        </div>
+
+        <div className="dateofgraduation">
+          <label htmlFor="datebootcamp" className="block text-sm font-medium text-gray-700">
+            Date of Graduation
+          </label>
+          <input
+            id="datebootcamp"
+            // {...register("datebootcamp")}
+            type="date"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            defaultValue={"2024-01-01"}
+          />
+        </div>
         </div>
 
     )
