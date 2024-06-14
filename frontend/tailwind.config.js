9b8ad30209bf1041345f434ed76997e6df86531f
luoxinnan <luoxinnan1@gmail.com>
2024-06-14 15:19:57 +0200

    Dark mode -
    Co-authored-by: william00771 <william.f.lindberg@hotmail.com>
    Co-authored-by: Zero <Zoldier777@users.noreply.github.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
-    extend: {    colors: {
+    extend: {
+      colors: {
       darkbg: '#2e2e38',
-      darkbg2: '#171722'
-    }},
+      darkbg1: '#171722'
+    }
+  },
 
   },
   plugins: [],
