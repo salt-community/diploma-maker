ac5b229a83418a3c40ec4401cf1a3d74b213ac1f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-18 14:47:07 +0200

    add: Navbar -> TemplateCreator anchor

--- frontend/src/pages/shared/Navbar.tsx
+++ frontend/src/pages/shared/Navbar.tsx
@@ -8,7 +8,6 @@ export const NavBar = () => {
         <nav className="navbar">
             <ul className="navbar__menu">
                 <li className="navbar__item">
-                    
                     <Link to="/" className={"navbar__link " + (location.pathname == '/' && "active")}>
                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path className="stroke" d="M1.99609 8.5H11.4961" stroke="#ababba" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
@@ -27,6 +26,31 @@ export const NavBar = () => {
                             <path className="fill" d="M3 5.6A2.6 2.6 0 0 1 5.6 3h2.8A2.6 2.6 0 0 1 11 5.6v2.8A2.6 2.6 0 0 1 8.4 11H5.6A2.6 2.6 0 0 1 3 8.4V5.6ZM3 15.6A2.6 2.6 0 0 1 5.6 13h2.8a2.6 2.6 0 0 1 2.6 2.6v2.8A2.6 2.6 0 0 1 8.4 21H5.6A2.6 2.6 0 0 1 3 18.4v-2.8ZM13 5.6A2.6 2.6 0 0 1 15.6 3h2.8A2.6 2.6 0 0 1 21 5.6v2.8a2.6 2.6 0 0 1-2.6 2.6h-2.8A2.6 2.6 0 0 1 13 8.4V5.6ZM17 13a1 1 0 0 1 1 1v2h2a1 1 0 1 1 0 2h-2v2a1 1 0 1 1-2 0v-2h-2a1 1 0 1 1 0-2h2v-2a1 1 0 0 1 1-1Z" fill="#ababba"/>
                         </svg>
                         Dashboard
+                    </Link>
+                </li>
+                <li className="navbar__item">
+                    <Link to="/template-creator" className={"navbar__link " + ((location.pathname == '/template-creator' || location.pathname == '/Template-creator') && "active")}>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ababba">
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <g id="ic-editor-scale">
+                            <rect className="cls-1" x="19" y="1" width="4" height="4" rx="1.25"></rect>
+                            <rect className="cls-1" x="1" y="1" width="4" height="4" rx="1.25"></rect>
+                            <rect className="cls-1" x="19" y="19" width="4" height="4" rx="1.25"></rect>
+                            <rect className="cls-1" x="1" y="19" width="4" height="4" rx="1.25"></rect>
+                            <line className="cls-1" x1="5" y1="3" x2="19" y2="3"></line>
+                            <line className="cls-1" x1="21" y1="5" x2="21" y2="19"></line>
+                            <line className="cls-1" x1="19" y1="21" x2="5" y2="21"></line>
+                            <line className="cls-1" x1="3" y1="19" x2="3" y2="5"></line>
+                            <line className="cls-1" x1="7" y1="18" x2="12" y2="6"></line>
+                            <line className="cls-1" x1="17" y1="18" x2="12" y2="6"></line>
+                            <line className="cls-1" x1="8.67" y1="14" x2="15.33" y2="14"></line>
+                            </g>
+                        </g>
+                        </svg>
+
+                        TemplateCreator
                     </Link>
                 </li>
             </ul>
