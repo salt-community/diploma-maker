ded6507f23e3d52b79023f538ff6a94b6fb6c226
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-18 15:08:16 +0200

    add: TemplateCreatorPage -> Bare Bones mockup

--- frontend/src/pages/TemplateCreatorPage.tsx
+++ frontend/src/pages/TemplateCreatorPage.tsx
@@ -1,11 +1,96 @@
+import { useEffect, useState } from "react";
+import { PublishButton } from "../components/MenuItems/Buttons/PublishButton";
+import { SelectOptions } from "../components/MenuItems/Inputs/SelectOptions";
 import { BootcampResponse } from "../util/types";
+import './TemplateCreatorPage.css'
+import { Template } from "@pdfme/common";
 
 type Props = {
     bootcamps: BootcampResponse[] | null;
 }
 
 export const TemplateCreatorPage = ({ bootcamps }: Props) => {
+    const [templates, setTemplates] = useState<Template>();
+
+    useEffect(() => {
+
+    })
+    const handleBootcampChange = () => {
+
+    }
+    const generatePDFsHandler = () => {
+
+    }
     return(
-        <h1>TemplateCreatorPage</h1>
+        <main className="templatecreator-page">
+            <section className='templatecreator-page__leftsidebar'>
+                <div className='templatecreator-page__leftsidebar-menu'>
+                    <header className="templatecreator-page__leftsidebar-menu-header">
+                        <button>
+                            Browse
+                        </button>
+                    </header>
+                    <section className="templatecreator-page__leftsidebar-menu-section">
+                        <h3>Filtering</h3>               </section>
+                    <section className="templatecreator-page__leftsidebar-menu-section">
+                        <h3>Bootcamps</h3>
+                        <SelectOptions
+                            containerClassOverride='overview-page__select-container'
+                            selectClassOverride='overview-page__select-box'
+                            options={[
+                                { value: "", label: "All Bootcamps" },
+                                ...(bootcamps?.map(bootcamp => ({
+                                    value: bootcamp.guidId,
+                                    label: bootcamp.name
+                                })) || [])
+                            ]}
+                            onChange={handleBootcampChange}
+                        />
+                    </section>
+                    <section className="templatecreator-page__leftsidebar-menu-section">
+                        <h3>Generate</h3>
+                        <PublishButton text='Generate PDFs' onClick={generatePDFsHandler} />
+                    </section>
+                </div>
+            </section>
+            <section className='templatecreator-page__preview-container'>
+                <div className='templatecreator-page__preview'>
+                    <h2>Slide 1</h2>
+                    <div className="pdfpreview">
+                        
+                    </div>
+                </div>
+            </section>
+            <section className='templatecreator-page__rightsidebar'>
+                <div className='templatecreator-page__rightsidebar-menu'>
+                    <header className="templatecreator-page__rightsidebar-menu-header">
+                        <button>
+                            Browse
+                        </button>
+                    </header>
+                    <section className="templatecreator-page__rightsidebar-menu-section">
+                        <h3>Filtering</h3>               </section>
+                    <section className="templatecreator-page__rightsidebar-menu-section">
+                        <h3>Bootcamps</h3>
+                        <SelectOptions
+                            containerClassOverride='overview-page__select-container'
+                            selectClassOverride='overview-page__select-box'
+                            options={[
+                                { value: "", label: "All Bootcamps" },
+                                ...(bootcamps?.map(bootcamp => ({
+                                    value: bootcamp.guidId,
+                                    label: bootcamp.name
+                                })) || [])
+                            ]}
+                            onChange={handleBootcampChange}
+                        />
+                    </section>
+                    <section className="templatecreator-page__rightsidebar-menu-section">
+                        <h3>Generate</h3>
+                        <PublishButton text='Generate PDFs' onClick={generatePDFsHandler} />
+                    </section>
+                </div>
+            </section>
+        </main>
     )
 }
