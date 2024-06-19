08d7036b6bb5768b62d43e68bbd3b68a1483268e
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-19 15:21:34 +0200

    fix: ConfirmationPopup same class as Popup

--- frontend/src/components/MenuItems/Popups/ConfirmationPopup.tsx
+++ frontend/src/components/MenuItems/Popups/ConfirmationPopup.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './ConfirmationPopup.css';
 
 export enum PopupType {
@@ -15,21 +14,19 @@ type Props = {
 }
 
 export const ConfirmationPopup = ({ show, popupType, title, text, confirmClick, abortClick }: Props) => {
-  const [visible, setVisible] = useState(show);
-
   return (
-    <div className={`popup ${popupType === PopupType.question ? 'question' : 'success'} ${visible ? 'fade-in' : 'fade-out'}`}>
-      <div className="popup-content">
+    <div className={`popup_confirmation ${popupType === PopupType.question ? 'question' : 'success'} ${show ? 'fade-in' : 'fade-out'}`}>
+      <div className="popup_confirmation-content">
         {popupType === PopupType.question && 
           <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
             <path fill-rule="evenodd" d="M11 11H9v-.148c0-.876.306-1.499 1-1.852.385-.195 1-.568 1-1a1.001 1.001 0 00-2 0H7c0-1.654 1.346-3 3-3s3 1 3 3-2 2.165-2 3zm-2 4h2v-2H9v2zm1-13a8 8 0 100 16 8 8 0 000-16z" fill="#FF683F"></path></g>
           </svg>
         }
-        <div className='popup-content-text'>
+        <div className='popup_confirmation-content-text'>
           <h1>{title}</h1>
           <p>{text}</p>
-          <div className='popup-content-btn-container'>
-            <button onClick={confirmClick} className='popup-content-btn'>
+          <div className='popup_confirmation-content-btn-container'>
+            <button onClick={confirmClick} className='popup_confirmation-content-btn'>
               <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="" stroke="" strokeWidth="0.01024">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
@@ -39,7 +36,7 @@ export const ConfirmationPopup = ({ show, popupType, title, text, confirmClick, 
               </svg>
               Yes
             </button>
-            <button onClick={abortClick} className='popup-content-btn'>
+            <button onClick={abortClick} className='popup_confirmation-content-btn'>
               <svg viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
