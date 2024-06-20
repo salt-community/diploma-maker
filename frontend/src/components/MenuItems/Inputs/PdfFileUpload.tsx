de5383ddaf31a9c1f2b919dcd19429fd50473479
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-20 16:17:26 +0200

    fix: PdfFileUpload -> Change file displaying despite incorrect file format

--- frontend/src/components/MenuItems/Inputs/PdfFileUpload.tsx
+++ frontend/src/components/MenuItems/Inputs/PdfFileUpload.tsx
@@ -12,10 +12,20 @@ export const PdfFileUpload = ({ fileResult, setFileAdded, fileAdded }: Props) =>
   const [isFileValid, setIsFileValid] = useState<boolean | null>(null);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    fileResult(acceptedFiles[0]);
-    setIsFileValid(null); 
-    if (setFileAdded) {
-      setFileAdded(true);
+    const file = acceptedFiles[0];
+    const isValid = file.type === 'application/pdf';
+
+    setIsFileValid(isValid);
+
+    if (isValid) {
+      fileResult(file);
+      if (setFileAdded) {
+        setFileAdded(true);
+      }
+    } else {
+      if (setFileAdded) {
+        setFileAdded(false);
+      }
     }
   }, [fileResult, setFileAdded]);
 
@@ -49,7 +59,7 @@ export const PdfFileUpload = ({ fileResult, setFileAdded, fileAdded }: Props) =>
     <div className="fileupload-wrapper" {...getRootProps()}>
       <input {...getInputProps()} />
         <>
-          <div className={'fileupload__icon-wrapper ' + (isDragActive ? (isFileValid ? 'valid ' : 'invalid ') : 'normal ') + (fileAdded && ' fileadded')}>
+          <div className={'fileupload__icon-wrapper ' + (fileAdded ? ' fileadded' : isDragActive ? (isFileValid ? 'valid ' : 'invalid ') : 'normal ')}>
             {fileAdded ? 
               <svg fill="#FFFFFFDF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
                 <path d="M23,12A11,11,0,1,1,12,1a10.9,10.9,0,0,1,5.882,1.7l1.411-1.411A1,1,0,0,1,21,2V6a1,1,0,0,1-1,1H16a1,1,0,0,1-.707-1.707L16.42,4.166A8.9,8.9,0,0,0,12,3a9,9,0,1,0,9,9,1,1,0,0,1,2,0Z"></path></g>
