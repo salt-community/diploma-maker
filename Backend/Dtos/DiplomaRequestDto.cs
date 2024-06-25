ae18c4842db5fe42e09a9272c842358571c4d489
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-25 10:31:00 +0200

    add: Update Models -> Email prop field

--- Backend/Dtos/DiplomaRequestDto.cs
+++ Backend/Dtos/DiplomaRequestDto.cs
@@ -4,5 +4,6 @@ public class DiplomaRequestDto
 {
     public Guid GuidId { get; set; } = Guid.NewGuid();
     public required string StudentName { get; set; }
+    public required string Email { get; set; }
     public required string BootcampGuidId {get; set;}
 }
--- Backend/Dtos/DiplomaResponseDto.cs
+++ Backend/Dtos/DiplomaResponseDto.cs
@@ -4,5 +4,6 @@ public class DiplomaResponseDto
 {
     public Guid GuidId { get; set; } = Guid.NewGuid();
     public required string StudentName { get; set; }
+    public required string Email { get; set; }
     public required BootcampInDiplomaDto Bootcamp {get; set;}
 }
