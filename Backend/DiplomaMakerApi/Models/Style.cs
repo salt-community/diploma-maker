8b6f5a4cc8fda1663300fab05c55a179cce19b9d
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-06-27 13:44:44 +0200

    add: Style Model

---
+++ Backend/DiplomaMakerApi/Models/Style.cs
@@ -0,0 +1,14 @@
+namespace DiplomaMakerApi.Models
+{
+    public class Style
+    {
+        public double? XPos { get; set; }
+        public double? YPos { get; set; }
+        public double? Width { get; set; }
+        public double? Height { get; set; }
+        public double? FontSize { get; set; }
+        public string? FontColor { get; set; }
+        public string? FontName { get; set; }
+        public string? Alignment { get; set; }
+    }
+}
