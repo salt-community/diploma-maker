8f06cf1dee0d033fa0ad61c92bb99d1e1c52daef
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-03 12:05:48 +0200

    add: Editsection -> Basic Styling

--- frontend/src/components/MenuItems/Inputs/EditSection.tsx
+++ frontend/src/components/MenuItems/Inputs/EditSection.tsx
@@ -2,28 +2,45 @@ import { useState } from "react";
 import DraggableInput from "./DraggableInput";
 import './EditSection.css'
 
-export const EditSection = () => {
-  const [position1, setPosition1] = useState<number>(5);
-  const [position2, setPosition2] = useState<number>(5);
-  const [size1, setSize1] = useState<number>(5);
-  const [size2, setSize2] = useState<number>(5);
+
+type Props = {
+  positionX: number,
+  setPositionX: (value: number) => void,
+  positionY: number,
+  setPositionY: (value: number) => void,
+  sizeWidth: number,
+  setSizeWidth: (value: number) => void,
+  sizeHeight: number,
+  setSizeHeight: (value: number) => void,
+}
+
+export const EditSection = ({ positionX, setPositionX, positionY, setPositionY, sizeWidth, setSizeWidth, sizeHeight, setSizeHeight }: Props) => {
+  const [lockAspectRatio, setLockAspectRatio] = useState<boolean>(false);
 
   return (
     <>
       
       <form className="editlayout__container" action="">
         <div className="editlayout__menusection">
-          <label htmlFor="">Position</label>
-          <div className="editlayout__menusection--inputwrapper">
-            <DraggableInput value={position1} setValue={setPosition1} label="X"/>
-            <DraggableInput value={position2} setValue={setPosition2} label="Y"/>
+          <div className="editlayout__menusection--title"> 
+            <label htmlFor="">Position</label>
           </div>
-          
+          <div className="editlayout__menusection--inputwrapper">
+            <DraggableInput value={positionX} setValue={setPositionX} label="X"/>
+            <DraggableInput value={positionY} setValue={setPositionY} label="Y"/>
+          </div>
         </div>
         <div className="editlayout__menusection">
-          <label htmlFor="">Size</label>
-          {/* <DraggableInput value={size1} setValue={setSize1} label="W"/>
-          <DraggableInput value={size2} setValue={setSize2} label="H"/> */}
+          <div className="editlayout__menusection--title">
+            <label htmlFor="">Size</label>
+            <svg onClick={() => setLockAspectRatio(!lockAspectRatio)} className={lockAspectRatio ? 'locked' : ''} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+              <path d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g>
+            </svg>
+          </div>
+          <div className="editlayout__menusection--inputwrapper">
+            <DraggableInput value={sizeWidth} setValue={setSizeWidth} label="W"/>
+            <DraggableInput value={sizeHeight} setValue={setSizeHeight} label="H"/>
+          </div>
         </div>
       </form>
     </>
