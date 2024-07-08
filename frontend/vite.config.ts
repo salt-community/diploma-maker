ddcf2591116e813df5f71e943ba7cfa61e411348
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-08 12:13:45 +0200

    deployment: -> port 5260

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5260,
+    port: 5260
   },
+  preview: {
+    port: 5260
+  }
 });
