47ffc2058194da6215d563711c05e92ef883240d
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-08 11:29:01 +0200

    disable: cloudbuilder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
-    strictPort: true
-  }
-})
+    port: Number(process.env.PORT) || 8080,
+    strictPort: true,
+  },
+});
