1d16718b2e0cc8b2bc3f13cd9db62c580a2b3a4a
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-08 11:59:54 +0200

    change: remap port 80 to 5260

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 8080,
-    strictPort: true,
+    port: 5260,
   },
 });
