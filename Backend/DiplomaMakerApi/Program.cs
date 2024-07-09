6a103f0691ed1ecf0462fa459e82598a1c2ce19f
Zoldier777 <Zzer0ph@gmail.com>
2024-07-09 14:44:47 +0200

    fix: update backend port

--- Backend/DiplomaMakerApi/Program.cs
+++ Backend/DiplomaMakerApi/Program.cs
@@ -3,10 +3,18 @@ using JokesAPI.Configuration;
 using DiplomaMakerApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel(options =>
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080"; 
+    options.ListenAnyIP(int.Parse(port));
+});
 /* builder.Services.AddDbContext<DiplomaMakingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DiplomaMakingContext") ?? throw new InvalidOperationException("Connection string 'DiplomaMakingContext' not found.")));
  */
-string connectionstr = builder.Environment.IsDevelopment() ? builder.Configuration.GetConnectionString("PostgreSQLConnectionLocal") : builder.Configuration.GetConnectionString("PostgreSQLConnection");
+string? connectionstr = builder.Environment.IsDevelopment() ?
+                        builder.Configuration.GetConnectionString("PostgreSQLConnectionLocal") :
+                        builder.Configuration.GetConnectionString("PostgreSQLConnection");
 
 builder.Services.AddDbContext<DiplomaMakingContext>(options =>
     options.UseNpgsql(connectionstr ?? throw new InvalidOperationException("Connection string 'DiplomaMakingContext' not found.")));
@@ -26,17 +34,20 @@ builder.Services.AddLogging();
 
 var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    SeedData.Initialize(services);
-}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+{   
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        SeedData.Initialize(services);
+    }
+  
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
