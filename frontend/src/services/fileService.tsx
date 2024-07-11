1e13212d5ae8008862ae713a9a4f404287a4e1f0
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-11 11:18:40 +0200

    add: fileService -> Saves file in localstorage

--- frontend/src/services/fileService.tsx
+++ frontend/src/services/fileService.tsx
@@ -1,16 +1,26 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export async function getTemplatePdfFile(url: string): Promise<string> {
+    const localStorageURL = `pdf_${url}`;
+    const cachedPdf = localStorage.getItem(localStorageURL);
+
+    if (cachedPdf) {
+        return cachedPdf;
+    }
+
     const pdfResponse = await fetch(`${apiUrl}/api/${url}`);
     if (!pdfResponse.ok) {
         throw new Error(`Failed to fetch PDF from ${url}`);
     }
 
     const pdfBlob = await pdfResponse.blob();
-    return await new Promise<string>((resolve, reject) => {
+    const pdfData = await new Promise<string>((resolve, reject) => {
         const reader = new FileReader();
         reader.onloadend = () => resolve(reader.result as string);
         reader.onerror = reject;
         reader.readAsDataURL(pdfBlob);
     });
+
+    localStorage.setItem(localStorageURL, pdfData);
+    return pdfData;
 }
