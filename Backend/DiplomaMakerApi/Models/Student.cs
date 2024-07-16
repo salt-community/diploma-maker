68bda166021895689e2cd741bfce5816275be8ce
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-16 14:12:04 +0200

    fix: Student Model -> typo

--- Backend/DiplomaMakerApi/Models/Student.cs
+++ Backend/DiplomaMakerApi/Models/Student.cs
@@ -8,5 +8,5 @@ public class Student
     public required string Name { get; set; }
     public string? Email { get; set; }
     [JsonIgnore]
-    public Bootcamp Bootcamp {get; set;}
+    public required Bootcamp Bootcamp {get; set;}
 }
