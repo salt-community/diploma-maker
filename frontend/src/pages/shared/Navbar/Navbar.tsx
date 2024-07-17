22185975447c1e1dd4f18951804fc63898101817
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-17 11:23:58 +0200

    add: Hide navbar if on verify page

--- frontend/src/pages/shared/Navbar/Navbar.tsx
+++ frontend/src/pages/shared/Navbar/Navbar.tsx
@@ -7,34 +7,39 @@ import { CogWheelIcon } from "../../../components/MenuItems/Icons/CogWheelIcon";
 
 export const NavBar = () => {
     const location = useLocation();
+    const hideNavItems = location.pathname.startsWith('/verify');
 
     return (
         <nav className="navbar">
             <ul className="navbar__menu">
-                <li className="navbar__item">
-                    <Link to="/overview" className={"navbar__link " + ((location.pathname == '/overview' || location.pathname == '/Overview') && "active")}>
-                        <DashBoardIcon />
-                        Dashboard
-                    </Link>
-                </li>
-                <li className="navbar__item">
-                    <Link to="/" className={"navbar__link " + (location.pathname == '/' && "active")}>
-                        <PdfCreatorIcon />
-                        PDFcreator
-                    </Link>
-                </li>
-                <li className="navbar__item">
-                    <Link to="/template-creator" className={"navbar__link " + ((location.pathname == '/template-creator' || location.pathname == '/Template-creator') && "active")}>
-                        <TemplateCreatorIcon />
-                        TemplateCreator
-                    </Link>
-                </li>
-                <li className="navbar__item">
-                    <Link to="/bootcamp-management" className={"navbar__link " + ((location.pathname == '/bootcamp-management' || location.pathname == '/Bootcamp-management') && "active")}>
-                        <CogWheelIcon />
-                        BootcampOptions
-                    </Link>
-                </li>
+                {!hideNavItems && (
+                    <>
+                        <li className="navbar__item">
+                            <Link to="/overview" className={"navbar__link " + ((location.pathname === '/overview' || location.pathname === '/Overview') && "active")}>
+                                <DashBoardIcon />
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li className="navbar__item">
+                            <Link to="/" className={"navbar__link " + (location.pathname === '/' && "active")}>
+                                <PdfCreatorIcon />
+                                PDFcreator
+                            </Link>
+                        </li>
+                        <li className="navbar__item">
+                            <Link to="/template-creator" className={"navbar__link " + ((location.pathname === '/template-creator' || location.pathname === '/Template-creator') && "active")}>
+                                <TemplateCreatorIcon />
+                                TemplateCreator
+                            </Link>
+                        </li>
+                        <li className="navbar__item">
+                            <Link to="/bootcamp-management" className={"navbar__link " + ((location.pathname === '/bootcamp-management' || location.pathname === '/Bootcamp-management') && "active")}>
+                                <CogWheelIcon />
+                                BootcampOptions
+                            </Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     );
