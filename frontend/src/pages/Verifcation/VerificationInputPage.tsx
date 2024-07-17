e32c11b6338011b7c8183fbd6df2f3d2672ba26f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-17 10:01:25 +0200

    add: VerificationInputPage -> routing to verify page

--- frontend/src/pages/Verifcation/VerificationInputPage.tsx
+++ frontend/src/pages/Verifcation/VerificationInputPage.tsx
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import { SaveButton } from '../../components/MenuItems/Buttons/SaveButton';
 import { VerifyIcon } from '../../components/MenuItems/Icons/VerifyIcon';
 import './VerificationInputPage.css'
+import { AlertPopup } from '../../components/MenuItems/Popups/AlertPopup';
+import { useCustomAlert } from '../../components/Hooks/useCustomAlert';
+import { useNavigate } from 'react-router-dom';
 
 export function VerificationInputPage() {
-        return (
-            <>
-                <div className='verificationinfo-container'>
-                    <div className='verificationinfo__logo-wrapper'>
-                        <img src="https://talent.salt.dev/logoBlack.png" alt="" />
-                    </div>
-                    <div className='verificationinfo__title-wrapper'>
-                        <p>Please put in your diploma verification code.</p>
-                        <input type="text" />¨
-                        <SaveButton classNameOverride='verificationinfo__title-wrapper--savebtn' textfield='Verify' customIcon={<VerifyIcon />} onClick={() => {}} saveButtonType={'normal'}/>
-                    </div>
+    const [inputContent, setInputContent] = useState<string>('');
+    const {showPopup, popupContent, popupType, customAlert, closeAlert } = useCustomAlert();
+    const navigate = useNavigate();
+
+    const submitHandler = () => {
+        if(inputContent.trim() !== ''){
+            navigate(`/verify/${inputContent}`);
+        }
+        else{
+            customAlert('fail',"Validation Error",`Please make sure the field is not empty`);
+        }
+    }
+
+    return (
+        <>
+            <AlertPopup
+                title={popupContent[0]}
+                text={popupContent[1]}
+                popupType={popupType}
+                show={showPopup}
+                onClose={closeAlert}
+            />
+            <div className='verificationinfo-container'>
+                <div className='verificationinfo__logo-wrapper'>
+                    <img src="https://talent.salt.dev/logoBlack.png" alt="" />
                 </div>
-            </>
-        );
+                <div className='verificationinfo__title-wrapper'>
+                    <p>Please put in your diploma verification code.</p>
+                    <input type="text" onChange={(e) => setInputContent(e.target.value)} value={inputContent}/>
+                    <SaveButton classNameOverride='verificationinfo__title-wrapper--savebtn' textfield='Verify' customIcon={<VerifyIcon />} onClick={() => submitHandler()} saveButtonType={'normal'}/>
+                </div>
+            </div>
+        </>
+    );
     }
