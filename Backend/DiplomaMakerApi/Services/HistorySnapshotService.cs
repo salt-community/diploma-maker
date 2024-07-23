8149ad864fe480e70935e34339b568d49cac97ca
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-23 13:51:38 +0200

    refractor: BE HistorySnapshotService -> Use Automapper for mapping values for less boiler plate code

--- Backend/DiplomaMakerApi/Services/HistorySnapshotService.cs
+++ Backend/DiplomaMakerApi/Services/HistorySnapshotService.cs
@@ -1,14 +1,16 @@
 using System.Linq;
+using AutoMapper;
 using DiplomaMakerApi.Exceptions;
 using DiplomaMakerApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DiplomaMakerApi.Services
 {
-    public class HistorySnapshotService(DiplomaMakingContext context, LocalFileStorageService localFileStorageService)
+    public class HistorySnapshotService(DiplomaMakingContext context, LocalFileStorageService localFileStorageService, IMapper mapper)
     {
         private readonly DiplomaMakingContext _context = context;
         private readonly LocalFileStorageService _localFileStorageService = localFileStorageService;
+        private readonly IMapper _mapper = mapper;
 
         public async Task CreateHistorySnapshotFromBootcamp(BootcampRequestUpdateDto requestDto, Bootcamp bootcampUsed) 
         {
@@ -45,70 +47,18 @@ namespace DiplomaMakerApi.Services
 
                 var timeUtcNow = DateTime.UtcNow;
                 
-                foreach(var student in requestDto.students){
-                    var studentSnapshot = new DiplomaSnapshot()
-                    {
-                        GeneratedAt = timeUtcNow,
-                        BootcampName = bootcampUsed.Name,
-                        BootcampGuidId = bootcampUsed.GuidId,
-                        BootcampGraduationDate = bootcampUsed.GraduationDate,
-                        StudentGuidId = student.GuidId,
-                        StudentName = student.Name,
-                        VerificationCode = student.VerificationCode,
-                        TemplateName = templateUsed?.Name,
-                        Intro = templateUsed?.Intro,
-                        IntroStyling = templateUsed?.IntroStyling == null ? null : new TemplateStyle()
+                foreach(var student in requestDto.students)
+                {
+                    var studentSnapshot = _mapper.Map<DiplomaSnapshot>(student, opt => 
                         {
-                            XPos = templateUsed.IntroStyling.XPos ?? null,
-                            YPos = templateUsed.IntroStyling.YPos ?? null,
-                            Width = templateUsed.IntroStyling.Width ?? null,
-                            Height = templateUsed.IntroStyling.Height ?? null,
-                            FontSize = templateUsed.IntroStyling.FontSize ?? null,
-                            FontColor = templateUsed.IntroStyling.FontColor ?? null,
-                            FontName = templateUsed.IntroStyling.FontName ?? null,
-                            Alignment = templateUsed.IntroStyling.Alignment ?? null,
-                        },
-                        Main = templateUsed?.Main,
-                        MainStyling = templateUsed?.MainStyling == null ? null : new TemplateStyle()
-                        {
-                            XPos = templateUsed.MainStyling.XPos ?? null,
-                            YPos = templateUsed.MainStyling.YPos ?? null,
-                            Width = templateUsed.MainStyling.Width ?? null,
-                            Height = templateUsed.MainStyling.Height ?? null,
-                            FontSize = templateUsed.MainStyling.FontSize ?? null,
-                            FontColor = templateUsed.MainStyling.FontColor ?? null,
-                            FontName = templateUsed.MainStyling.FontName ?? null,
-                            Alignment = templateUsed.MainStyling.Alignment ?? null,
-                        },
-                        Footer = templateUsed?.Footer,
-                        FooterStyling = templateUsed?.FooterStyling == null ? null : new TemplateStyle()
-                        {
-                            XPos = templateUsed.FooterStyling.XPos ?? null,
-                            YPos = templateUsed.FooterStyling.YPos ?? null,
-                            Width = templateUsed.FooterStyling.Width ?? null,
-                            Height = templateUsed.FooterStyling.Height ?? null,
-                            FontSize = templateUsed.FooterStyling.FontSize ?? null,
-                            FontColor = templateUsed.FooterStyling.FontColor ?? null,
-                            FontName = templateUsed.FooterStyling.FontName ?? null,
-                            Alignment = templateUsed.FooterStyling.Alignment ?? null,
-                        },
-                        Link = templateUsed?.Link,
-                        LinkStyling = templateUsed?.LinkStyling == null ? null : new TemplateStyle()
-                        {
-                            XPos = templateUsed.LinkStyling.XPos ?? null,
-                            YPos = templateUsed.LinkStyling.YPos ?? null,
-                            Width = templateUsed.LinkStyling.Width ?? null,
-                            Height = templateUsed.LinkStyling.Height ?? null,
-                            FontSize = templateUsed.LinkStyling.FontSize ?? null,
-                            FontColor = templateUsed.LinkStyling.FontColor ?? null,
-                            FontName = templateUsed.LinkStyling.FontName ?? null,
-                            Alignment = templateUsed.LinkStyling.Alignment ?? null,
-                        },
-                        BasePdf = templateBackgroundLocation,
-                        TemplateLastUpdated = templateUsed?.LastUpdated ?? default(DateTime),
-                        BasePdfBackgroundLastUpdated = templateUsed?.PdfBackgroundLastUpdated ?? default(DateTime),
-                        Active = lastSnapshot == null || !lastSnapshots.Any(s => s.StudentGuidId == student.GuidId)
-                    };
+                            opt.Items["bootcampUsed"] = bootcampUsed;
+                            opt.Items["templateUsed"] = templateUsed;
+                            opt.Items["templateBackgroundLocation"] = templateBackgroundLocation;
+                            opt.Items["lastSnapshot"] = lastSnapshot;
+                            opt.Items["lastSnapshots"] = lastSnapshots;
+                            opt.Items["timeUtcNow"] = timeUtcNow;
+                        });
+                    studentSnapshot.GeneratedAt = timeUtcNow;
                     _context.DiplomaSnapshots.Add(studentSnapshot);
                     await _context.SaveChangesAsync();
                 }
