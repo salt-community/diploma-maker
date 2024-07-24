57ac96b29c4a3849765ab788f672a7e4e5d3d739
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-24 15:15:55 +0200

    add: BE update bootcamprequestdto

--- Backend/DiplomaMakerApi/Dtos/Bootcamp/BootcampRequestDto.cs
+++ Backend/DiplomaMakerApi/Dtos/Bootcamp/BootcampRequestDto.cs
@@ -3,6 +3,7 @@ namespace DiplomaMakerApi.Dtos;
 
 public class BootcampRequestDto
 {
+    public required string Name { get; set; }
     public required int TrackId { get; set; }
     public required DateTime GraduationDate { get; set; } 
 }
