63c8886ccd20f2e29c124259281fc53796ba7fd6
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-30 16:24:49 +0200

    add: TemplateService -> if development localstorage vs googlestorage use

--- Backend/DiplomaMakerApi/Services/TemplateService.cs
+++ Backend/DiplomaMakerApi/Services/TemplateService.cs
@@ -38,9 +38,11 @@ public class TemplateService
         {
             Name = templateRequest.templateName,
         };
-       
-        // await _localFileStorageService.InitFileFromNewTemplate(templateRequest.templateName);
-        await _googleCloudStorageService.InitFileFromNewTemplate(templateRequest.templateName);
+
+        await (!_env.IsDevelopment()
+            ? _localFileStorageService.InitFileFromNewTemplate(templateRequest.templateName)
+            : _googleCloudStorageService.InitFileFromNewTemplate(templateRequest.templateName));
+
         await _context.DiplomaTemplates.AddAsync(newTemplate);
         await _context.SaveChangesAsync();
         return newTemplate;
@@ -68,8 +70,11 @@ public class TemplateService
         template.PdfBackgroundLastUpdated = templateRequest.PdfBackgroundLastUpdated;
 
         IFormFile file = ConvertBase64ToIFormFile(templateRequest.basePdf, templateRequest.templateName);
-        // await _localFileStorageService.SaveFile(file, templateRequest.templateName);
-        await _googleCloudStorageService.SaveFile(file, templateRequest.templateName);
+
+        await (!_env.IsDevelopment()
+            ? _localFileStorageService.SaveFile(file, templateRequest.templateName)
+            : _googleCloudStorageService.SaveFile(file, templateRequest.templateName));
+
         _context.DiplomaTemplates.Update(template);
         await _context.SaveChangesAsync();
 
@@ -84,8 +89,10 @@ public class TemplateService
             throw new NotFoundByIdException("Template", id);
         }
 
-        // await _localFileStorageService.DeleteFile(template.Name);
-        await _googleCloudStorageService.DeleteFile(template.Name);
+        await (_env.IsDevelopment() 
+            ? _localFileStorageService.DeleteFile(template.Name)
+            : _googleCloudStorageService.DeleteFile(template.Name));
+
         _context.DiplomaTemplates.Remove(template);
         await _context.SaveChangesAsync();
 
