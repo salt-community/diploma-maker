00479dc0f3c55cbc90d7f3b4903bb4353323b199
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-30 23:05:06 +0200

    add: EmailClient -> TextArea for description & styling

--- frontend/src/components/EmailClient/EmailClient.tsx
+++ frontend/src/components/EmailClient/EmailClient.tsx
@@ -199,9 +199,8 @@ export const EmailClient = ({ clients, title, show, closeEmailClient, modifyStud
                                 <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="var(--ci-primary-color, #ababba)" d="M334.627,16H48V496H472V153.373ZM440,166.627V168H320V48h1.373ZM80,464V48H288V200H440V464Z" className="ci-primary"></path> <rect width="224" height="32" x="136" y="296" fill="var(--ci-primary-color, #ababba)" className="ci-primary"></rect> <rect width="224" height="32" x="136" y="376" fill="var(--ci-primary-color, #ababba)" className="ci-primary"></rect> </g></svg>
                                 <div className="emailclient__list--input-wrapper editfields">
                                     <h3>Description</h3>
-                                    <input
+                                    <textarea
                                         className="emailclient__list--input editfields"
-                                        type="text"
                                         ref={emailDescriptionInput}
                                         onChange={handleEmailDescriptionChange}
                                         value={emailDescription}
