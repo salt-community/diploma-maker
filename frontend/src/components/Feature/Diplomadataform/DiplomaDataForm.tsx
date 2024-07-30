00a7f2945c3aede475be0f911752246c4c470b87
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-07-30 15:08:31 +0200

    fix: FE Track Selector -> now sets correct track index

--- frontend/src/components/Feature/Diplomadataform/DiplomaDataForm.tsx
+++ frontend/src/components/Feature/Diplomadataform/DiplomaDataForm.tsx
@@ -143,16 +143,12 @@ export default function DiplomaDataForm({ updateSaltData, tracks, bootcamps, set
           className="mt-2 w-8/12 py-2 px-3 order border-gray-300 dark:border-none bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-darkbg dark:text-white"
           onChange={
             (e) => {
-
               setTrackIndex(Number(e.target.value))
-              const selectedBootcamp = tracks[Number(e.target.value)].bootcamps[0];
+              const selectedBootcamp = tracks[Number(e.target.value) - 1].bootcamps[0];
               const selectedTemplateObject = templates.find(template => template.id === selectedBootcamp.templateId)
-              console.log(selectedBootcamp)
-              console.log(selectedTemplateObject)
               setSelectedTemplate(selectedTemplateObject);
               setStudents(selectedBootcamp.students);
             } 
-      
           }
         >
           {tracks && (
