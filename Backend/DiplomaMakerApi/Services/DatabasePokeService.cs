8c3c5e72df73f19c90701e48195516f576fd70c6
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-09 09:32:10 +0200

    add: DatabasePokeService

---
+++ Backend/DiplomaMakerApi/Services/DatabasePokeService.cs
@@ -0,0 +1,20 @@
+
+namespace DiplomaMakerApi.Services
+{
+    public class DatabasePokeService : IHostedService, IDisposable
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
