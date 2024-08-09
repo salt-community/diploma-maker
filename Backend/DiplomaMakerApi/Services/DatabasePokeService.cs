ccedcf72d5f939aa717cc55f510fb6915abccc77
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-09 09:46:54 +0200

    add: DatabasePokeService -> Set Connectionstring from program.cs

--- Backend/DiplomaMakerApi/Services/DatabasePokeService.cs
+++ Backend/DiplomaMakerApi/Services/DatabasePokeService.cs
@@ -1,58 +1,52 @@
-
 using Microsoft.EntityFrameworkCore;
 
-namespace DiplomaMakerApi.Services
+public class DatabasePokeService : IHostedService, IDisposable
 {
-    public class DatabasePokeService : IHostedService, IDisposable
+    private Timer? _timer;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly string _dbConnectionString;
+    public DatabasePokeService(IServiceProvider serviceProvider, string dbConnectionString)
     {
-        private Timer _timer = null!;
-        private readonly IServiceProvider _serviceProvider;
+        _serviceProvider = serviceProvider;
+        _dbConnectionString = dbConnectionString;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _timer = new Timer(PokeDatabase, null, TimeSpan.Zero, TimeSpan.FromHours(1));
+        return Task.CompletedTask;
+    }
+    private void PokeDatabase(object? state)
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<DiplomaMakingContext>();
 
-        public DatabasePokeService(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _timer = new Timer(PokeDatabase, null, TimeSpan.Zero, TimeSpan.FromHours(1));
-            return Task.CompletedTask;
-        }
-
-        private void PokeDatabase(object? state)
-        {
-            using (var scope = _serviceProvider.CreateScope())
+            try
             {
-                var context = scope.ServiceProvider.GetRequiredService<DiplomaMakingContext>();
-
-                try
+                if (string.IsNullOrEmpty(_dbConnectionString))
                 {
-                    var connectionString = Environment.GetEnvironmentVariable("PostgreConnection");
-                    if (string.IsNullOrEmpty(connectionString))
-                    {
-                        Console.WriteLine("Connection string not found.");
-                        return;
-                    }
-
-                    context.Database.SetConnectionString(connectionString);
-
-                    var result = context.Database.CanConnect();
-                    Console.WriteLine(result ? "Database Poke Complete." : "Failed to connect and poke database.");
+                    Console.WriteLine("Connection string not found.");
+                    return;
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"An error occurred while poking the database: {ex.Message}");
-                }
+
+                context.Database.SetConnectionString(_dbConnectionString);
+
+                var result = context.Database.CanConnect();
+                Console.WriteLine(result ? "Database Poke Complete." : "Failed to connect and poke database.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while poking the database: {ex.Message}");
             }
         }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _timer?.Change(Timeout.Infinite, 0);
-            return Task.CompletedTask;
-        }
-        public void Dispose()
-        {
-            _timer?.Dispose();
-        }
+    }
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer?.Change(Timeout.Infinite, 0);
+        return Task.CompletedTask;
+    }
+    public void Dispose()
+    {
+        _timer?.Dispose();
     }
 }
