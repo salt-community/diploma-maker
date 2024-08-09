48f2be9d3d7be55b11c30761b65ff9b89e8ad38f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-09 09:42:02 +0200

    add: DatabasePokeService -> implement PokeDatabase

--- Backend/DiplomaMakerApi/Services/DatabasePokeService.cs
+++ Backend/DiplomaMakerApi/Services/DatabasePokeService.cs
@@ -1,20 +1,58 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace DiplomaMakerApi.Services
 {
     public class DatabasePokeService : IHostedService, IDisposable
     {
+        private Timer _timer;
+        private readonly IServiceProvider _serviceProvider;
+
+        public DatabasePokeService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _timer = new Timer(PokeDatabase, null, TimeSpan.Zero, TimeSpan.FromHours(1));
+            return Task.CompletedTask;
+        }
+
+        private void PokeDatabase(object state)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DiplomaMakingContext>();
+
+                try
+                {
+                    var connectionString = Environment.GetEnvironmentVariable("PostgreConnection");
+                    if (string.IsNullOrEmpty(connectionString))
+                    {
+                        Console.WriteLine("Connection string not found.");
+                        return;
+                    }
+
+                    context.Database.SetConnectionString(connectionString);
+
+                    var result = context.Database.CanConnect();
+                    Console.WriteLine(result ? "Database Poke Complete." : "Failed to connect and poke database.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while poking the database: {ex.Message}");
+                }
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
         }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _timer?.Dispose();
         }
     }
 }
