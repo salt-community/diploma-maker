ca4c7a039591307f64a382bebbd68ab984193206
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-12 09:27:02 +0200

    abort: i give up, disable localstorage on pdfFiles (for now)

--- frontend/src/services/fileService.tsx
+++ frontend/src/services/fileService.tsx
@@ -1,17 +1,18 @@
 export async function getTemplatePdfFile(apiUrl: string, url: string, lastUpdated: Date, setLoadingMessage?: (message: string) => void): Promise<string> {
-    const localStorageKey = `pdf_${url}`;
-    const cachedPdf = localStorage.getItem(localStorageKey);
+    // const localStorageKey = `pdf_${url}`;
+    
+    // const cachedPdf = localStorage.getItem(localStorageKey);
 
-    if (cachedPdf) {
-        const { pdfData, dateAdded } = JSON.parse(cachedPdf);
-        if (new Date(lastUpdated).toISOString() === dateAdded) {
-            return pdfData;
-        }
-    }
+    // if (cachedPdf) {
+    //     const { pdfData, dateAdded } = JSON.parse(cachedPdf);
+    //     if (new Date(lastUpdated).toISOString() === dateAdded) {
+    //         return pdfData;
+    //     }
+    // }
 
     const pdfResponse = await fetch(`${apiUrl}/api/${url}`);
     if (!pdfResponse.ok) {
-        setLoadingMessage(`Failed to fetch PDF file from ${url}. The file does not seem to exist.`)
+        setLoadingMessage(`Failed to fetch PDF file from ${url}. The file does not seem to exist.`);
         throw new Error(`Failed to fetch PDF from ${url}`);
     }
 
@@ -28,24 +29,24 @@ export async function getTemplatePdfFile(apiUrl: string, url: string, lastUpdate
         dateAdded: new Date(lastUpdated).toISOString()
     };
 
-    try {
-        localStorage.setItem(localStorageKey, JSON.stringify(dataToStore));
-    } catch (e) {
-        if (e instanceof DOMException && e.name === 'QuotaExceededError') {
-            const keys = Object.keys(localStorage);
+    // try {
+    //     localStorage.setItem(localStorageKey, JSON.stringify(dataToStore));
+    // } catch (e) {
+    //     if (e instanceof DOMException && e.name === 'QuotaExceededError') {
+    //         const keys = Object.keys(localStorage);
 
-            for (let i = 0; i < keys.length; i++) {
-                const key = keys[i];
-                if (key.startsWith('pdf_')) {
-                    localStorage.removeItem(key);
-                    break;
-                }
-            }
-            localStorage.setItem(localStorageKey, JSON.stringify(dataToStore));
-        } else {
-            throw e;
-        }
-    }
+    //         for (let i = 0; i < keys.length; i++) {
+    //             const key = keys[i];
+    //             if (key.startsWith('pdf_')) {
+    //                 localStorage.removeItem(key);
+    //                 break;
+    //             }
+    //         }
+    //         localStorage.setItem(localStorageKey, JSON.stringify(dataToStore));
+    //     } else {
+    //         throw e;
+    //     }
+    // }
 
     return pdfData;
 }
