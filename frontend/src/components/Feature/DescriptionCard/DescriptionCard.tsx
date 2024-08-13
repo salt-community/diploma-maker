2860c826dbc1d72531128e827900150ddbc8a8ce
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-13 10:41:21 +0200

    add: DescriptionCard -> Smoother animations

--- frontend/src/components/Feature/DescriptionCard/DescriptionCard.tsx
+++ frontend/src/components/Feature/DescriptionCard/DescriptionCard.tsx
@@ -12,39 +12,33 @@ export function DescriptionCard({ shortDescription, LongDescription, title, icon
 
     return (
         <div
-            className="homepage__card"
-            onMouseEnter={() => {setIsHovered(true); onMouseEnter();}}
-            onMouseLeave={() => {setIsHovered(false); onMouseLeave();}}
+            className={`homepage__card ${isHovered ? 'visible' : ''}`}
+            onMouseEnter={() => { setIsHovered(true); onMouseEnter(); }}
+            onMouseLeave={() => { setIsHovered(false); onMouseLeave(); }}
         >
             <header className="homepage__card-header">
                 <Icon />
                 <h2>{title}</h2>
             </header>
             <div className="homepage__card-content">
-
-                {isHovered ? (
-                    <div className="homepage__card-full-description">
-                        <p className="homepage__card-full-description-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis numquam architecto aut laudantium modi nesciunt nobis exercitationem inventore et, praesentium illum asperiores impedit pariatur optio rem magni recusandae adipisci a, consectetur suscipit iusto ullam iure consequatur sunt? Magnam, magni corporis.
-                        </p>
-                        <div className="submit-button-container">
-                            <button
-                                className="submit-button"
-                            >
-                                Visual instructions
-                            </button>
-                        </div>
+                <div className={`homepage__card-full-description ${isHovered ? 'visible' : ''}`}>
+                    <p className="homepage__card-full-description-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis numquam architecto aut laudantium modi nesciunt nobis exercitationem inventore et, praesentium illum asperiores impedit pariatur optio rem magni recusandae adipisci a, consectetur suscipit iusto ullam iure consequatur sunt? Magnam, magni corporis.
+                    </p>
+                    <div className="submit-button-container">
+                        <button className="submit-button" onMouseUp={() => {alert("hi")}}>
+                            Visual instructions
+                        </button>
                     </div>
-                ) : (
-                    <div className="homepage__card-short-description">
-                        <ul className="short-description-list">
-                            {shortDescription.map((item, index) => (
-                                <li key={index}>{item}</li>
-                            ))}
-                        </ul>
-                        <span className="learn-more-text">Hover to learn more...</span>
-                    </div>
-                )}
+                </div>
+                <div className={`homepage__card-short-description ${isHovered ? 'hidden' : ''}`}>
+                    <ul className="short-description-list">
+                        {shortDescription.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                    <span className="learn-more-text">Hover to learn more...</span>
+                </div>
             </div>
         </div>
     );
