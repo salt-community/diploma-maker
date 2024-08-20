2763fea892b31582f265e6bef8a7ab4d4f51213f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-08-20 08:29:54 +0200

    add: BE UserFontsController

---
+++ Backend/DiplomaMakerApi/Controllers/UserFontsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiplomaMakerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserFontsController : ControllerBase
+    {
+        
+    }
+}
