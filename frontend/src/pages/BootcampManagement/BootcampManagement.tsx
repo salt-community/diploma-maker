8cf8015c81f28ea40e5189549bc951bf9273240d
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-06 10:18:14 +0200

    refractor: FE BootcampsManagement -> move bootcampsform -> BootcampsManageForm component

--- frontend/src/pages/BootcampManagement/BootcampManagement.tsx
+++ frontend/src/pages/BootcampManagement/BootcampManagement.tsx
@@ -1,17 +1,11 @@
 import './BootcampManagement.css'
-import { FieldValues, useForm } from "react-hook-form";
-import { BootcampsTable } from "../../components/Feature/BootcampManage/BootcampsTable";
-import AddNewBootcampForm from "../../components/Forms/AddNewBootcampForm";
-import { PaginationMenu } from "../../components/MenuItems/PaginationMenu";
 import { AlertPopup } from "../../components/MenuItems/Popups/AlertPopup";
 import { ConfirmationPopup } from "../../components/MenuItems/Popups/ConfirmationPopup";
 import { BootcampRequest, BootcampResponse, TrackResponse } from "../../util/types";
-import { useEffect, useRef, useState } from "react";
-import { utcFormatterSlash } from "../../util/datesUtil";
+import { useEffect, useState } from "react";
 import { useCustomAlert } from "../../components/Hooks/useCustomAlert";
 import { useCustomConfirmationPopup } from "../../components/Hooks/useCustomConfirmationPopup";
-import { BootcampsSectionHeader } from '../../components/Feature/BootcampManage/BootcampsSectionHeader';
-import { BootcampsSectionFooter } from '../../components/Feature/BootcampManage/BootcampsSectionFooter';
+import { BootcampsManageForm } from '../../components/Feature/BootcampManage/BootcampsManageForm';
 
 type Props = {
   deleteBootcamp: (i: number) => Promise<void>;
@@ -24,11 +18,6 @@ export default function BootcampManagement({ deleteBootcamp, addNewBootcamp, upd
   const [bootcamps, setBootcamps] = useState<BootcampResponse[] | null>(null);
   const [filteredBootcamps, setFilteredBootcamps] = useState<BootcampResponse[] | null>(null);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [sortingChanged, setSortingChanged] = useState(false);
-
-  const calendarPickers = useRef([]);
-  const { register, handleSubmit, setValue, watch, reset } = useForm();
   const { showConfirmationPopup, confirmationPopupContent, confirmationPopupType, confirmationPopupHandler, customPopup, closeConfirmationPopup } = useCustomConfirmationPopup();
   const { showPopup, popupContent, popupType, customAlert, closeAlert } = useCustomAlert();
 
@@ -39,39 +28,6 @@ export default function BootcampManagement({ deleteBootcamp, addNewBootcamp, upd
       setFilteredBootcamps(bootcampsArr);
     }
   }, [tracks])
-
-  const addNewBootcampHandler = async (bootcamp: BootcampRequest) => {
-      customAlert('loading', "Adding New Bootcamp...", ``);
-      try {
-          await addNewBootcamp(bootcamp)
-          customAlert('success', "Added Bootcamp Successfully.", `Successfully added bootcamp to the database.`);
-      } catch (error) {
-          customAlert('fail', "Error Adding Bootcamp", `${error}`);
-      }
-  }
-
-  const handleUpdateBootcamp = async (data: FieldValues) => {
-      closeConfirmationPopup();
-      customAlert('loading', "Updating Bootcamp...", ``);
-      for (let i = 0; i < bootcamps!.length; i++) {
-          const newBootcamp: BootcampRequest = {
-          guidId: bootcamps![i].guidId,
-          name: data[`name${i}`],
-          graduationDate: new Date(data[`dategraduate${i}`]),
-          trackId: parseInt(data[`track${i}`])
-          };
-
-          try {
-          await updateBootcamp(newBootcamp);
-          customAlert('success', "Updated Bootcamps Successfully.", `Successfully updated bootcamp in the database.`);
-          setSortingChanged(prev => !prev);
-          } catch (error) {
-          customAlert('fail', "Error Updating Bootcamp", `${error}`);
-          }
-      }
-  }
-
-  const confirmChangeBootcampHandler = async (data: FieldValues) => customPopup('question2', "Are you sure you want to change existing bootcamps?", "This can be destructive if you've already generated diplomas with that bootcamp.", () => () => handleUpdateBootcamp(data));
 
   return (
     <>
@@ -85,36 +41,18 @@ export default function BootcampManagement({ deleteBootcamp, addNewBootcamp, upd
         // @ts-ignore
         confirmClick={(inputContent?: string) => { confirmationPopupHandler(inputContent) }}
       />
-      <form onSubmit={handleSubmit(confirmChangeBootcampHandler)}>
-        <div className="modal-container">
-          <div className="modal-content">
-            <div className="modal-body">
-              <BootcampsTable 
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                sortingChanged={sortingChanged}
-                setSortingChanged={setSortingChanged}
-                tracks={tracks}
-                bootcamps={bootcamps}
-                filteredBootcamps={filteredBootcamps}
-                setFilteredBootcamps={setFilteredBootcamps}
-                deleteBootcamp={deleteBootcamp}
-                calendarPickers={calendarPickers}
-                closeConfirmationPopup={closeConfirmationPopup}
-                customAlert={customAlert}
-                customPopup={customPopup}
-                register={register}
-                setValue={setValue}
-                watch={watch}
-                reset={reset}
-              />
-              <AddNewBootcampForm addNewBootcamp={addNewBootcampHandler} bootcamps={bootcamps} tracks={tracks} />
-              <BootcampsSectionFooter />
-            </div>
-          </div>
-        </div>
-        <div className="overlay-bg"></div>
-      </form>
+      <BootcampsManageForm 
+        tracks={tracks}
+        bootcamps={bootcamps}
+        filteredBootcamps={filteredBootcamps}
+        setFilteredBootcamps={setFilteredBootcamps}
+        deleteBootcamp={deleteBootcamp}
+        addNewBootcamp={addNewBootcamp}
+        updateBootcamp={updateBootcamp}
+        closeConfirmationPopup={closeConfirmationPopup}
+        customAlert={customAlert}
+        customPopup={customPopup}
+      />
     </>
   );
 }
