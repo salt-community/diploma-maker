0c3120c5ac4c95d94fd1cdf7c8da93d5703ade3b
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-12 10:44:42 +0200

    add: DiplomaMakerApiFactory -> ClearProviders

--- Backend/DiplomaMakerApi.Tests.Integration/DiplomaMakerApiFactory.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/DiplomaMakerApiFactory.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
 
 namespace DiplomaMakerApi.Tests.Integration
 {
     public class DiplomaMakerApiFactory : WebApplicationFactory<IDiplomaApiMarker>
     {
-        
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging(logging => {
+                logging.ClearProviders();
+            });
+        }
     }
 }
