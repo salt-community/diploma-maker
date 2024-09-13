35c527621c8d69e31f81d9ee1035edee656cbf1e
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-13 10:12:35 +0200

    test: DeleteTemplatesControllerTests -> DeleteTemplate_ReturnsNoContent_WhenValidRequest

--- Backend/DiplomaMakerApi.Tests.Integration/TestUtil.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/TestUtil.cs
@@ -6,10 +6,20 @@ namespace DiplomaMakerApi.Tests.Integration
     {
         public static void CheckFileExists(string templateName, string extension, string subDirectory)
         {
-            var testProjectBinRoot = Directory.GetCurrentDirectory();
-            var expectedFilePath = Path.Combine(testProjectBinRoot, "Blob", subDirectory, $"{templateName}{extension}");
+            var filePath = GetFilePath(templateName, extension, subDirectory);
+            File.Exists(filePath).Should().BeTrue($"File {templateName}.{extension} should exist at {filePath}");
+        }
 
-            File.Exists(expectedFilePath).Should().BeTrue($"File {templateName}.{extension} should exist at {expectedFilePath}");
+        public static void CheckFileDoesNotExist(string templateName, string extension, string subDirectory)
+        {
+            var filePath = GetFilePath(templateName, extension, subDirectory);
+            File.Exists(filePath).Should().BeFalse($"File {templateName}.{extension} should not exist at {filePath}");
+        }
+
+        private static string GetFilePath(string templateName, string extension, string subDirectory)
+        {
+            var testProjectBinRoot = Directory.GetCurrentDirectory();
+            return Path.Combine(testProjectBinRoot, "Blob", subDirectory, $"{templateName}{extension}");
         }
     }
 }
