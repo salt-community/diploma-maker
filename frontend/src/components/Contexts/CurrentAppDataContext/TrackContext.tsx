38c154345c4ba5b8790be2115c5422d189fa71e9
Zoldier777 <Zzer0ph@gmail.com>
2024-09-17 10:48:28 +0200

    fix: change basepdf

---
+++ frontend/src/components/Contexts/CurrentAppDataContext/TrackContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+import { TrackResponse } from "../../../util/types";
+
+
+type TrackContextType = {
+  tracks: TrackResponse[];
+  currentTrackIndex: number;
+  setTracks: (tracks: TrackResponse[]) => void;
+  setCurrentTrackIndex: (index: number) => void;
+};
+
+//trackindex bootcampindex tracks  
+
+
+const TrackContext = createContext<TrackContextType | undefined>(undefined);
+
+export const useTrack = () => {
+  const context = useContext(TrackContext);
+  if (!context) throw new Error("useTrack must be used within a TrackProvider");
+  return context;
+};
+
+export const TrackProvider = ({ children }: { children: React.ReactNode }) => {
+  const [tracks, setTracks] = useState<TrackResponse[] | null>(null);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState<number | null>(null);
+
+  return (
+    <TrackContext.Provider value={{ tracks, currentTrackIndex, setTracks, setCurrentTrackIndex }}>
+      {children}
+    </TrackContext.Provider>
+  );
+};
