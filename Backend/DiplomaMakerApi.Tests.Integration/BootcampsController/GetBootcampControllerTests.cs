853cc399040d320cc86d22d4d6ec8f98517c564d
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-18 14:10:42 +0200

    test: GetBootcampsControllerTests -> GetBootcampByGuidId_ReturnsTemplate_WhenIdExists

--- Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/GetBootcampControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/GetBootcampControllerTests.cs
@@ -1,5 +1,9 @@
 using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Bogus;
+using DiplomaMakerApi.Dtos;
+using DiplomaMakerApi.Models;
 using FluentAssertions;
 using Xunit;
 
@@ -8,10 +12,39 @@ namespace DiplomaMakerApi.Tests.Integration.BootcampsController
     public class GetBootcampControllerTests : IClassFixture<DiplomaMakerApiFactory>
     {
         private readonly HttpClient _client;
+        private readonly Faker<BootcampRequestDto> _bootcampRequestGenerator = 
+            new Faker<BootcampRequestDto>()
+                .RuleFor(b => b.GraduationDate, _ => DateTime.UtcNow)
+                .RuleFor(b => b.TrackId, faker => faker.Random.Int(1, 3));
         public GetBootcampControllerTests(DiplomaMakerApiFactory apiFactory)
         {
             _client = apiFactory.CreateClient();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "test-token");
+        }
+
+        [Fact]
+        public async void GetBootcampByGuidId_ReturnsTemplate_WhenIdExists()
+        {
+            // Arrange
+            var trackIdToNameMap = new Dictionary<int, string>
+            {
+                { 1, "C# Dotnet" },
+                { 2, "Java" },
+                { 3, "Javascript" }
+            };
+            var setupBootcamp = _bootcampRequestGenerator.Generate();
+            var setupBootcampResponse = await TestUtil.CreateAndPostAsync<BootcampRequestDto, BootcampResponseDto>(
+                _client, setupBootcamp, "api/Bootcamps"
+            );
+
+            // Act
+            var response = await _client.GetAsync($"api/Bootcamps/{setupBootcampResponse.GuidId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var bootcampResponse = await response.Content.ReadFromJsonAsync<BootcampResponseDto>();
+            bootcampResponse!.GraduationDate.Date.Should().Be(setupBootcamp.GraduationDate.Date);
+            bootcampResponse.Track.Name.Should().Be(trackIdToNameMap[setupBootcamp.TrackId]);
         }
 
         [Fact]
