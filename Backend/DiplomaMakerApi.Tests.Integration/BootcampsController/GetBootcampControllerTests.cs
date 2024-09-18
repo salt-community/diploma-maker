53789040cf609464cd835bff621a7b14ec9cd312
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-18 14:02:43 +0200

    test: GetBootcampControllerTests -> GetBootcampByGuidId_ReturnsUnathorized_WhenInvalidToken

--- Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/GetBootcampControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/GetBootcampControllerTests.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.Http.Headers;
+using FluentAssertions;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.BootcampsController
@@ -10,6 +12,19 @@ namespace DiplomaMakerApi.Tests.Integration.BootcampsController
         {
             _client = apiFactory.CreateClient();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "test-token");
+        }
+
+        [Fact]
+        public async Task GetBootcampByGuidId_ReturnsUnathorized_WhenInvalidToken()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = null;
+
+            // Act
+            var response = await _client.GetAsync($"api/Templates/{32135}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
     }
 }
