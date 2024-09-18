85584f8046d0bc5b197001e2c1c8f9c966cc0564
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-18 15:32:59 +0200

    test: UpdateBootcampControllerTests -> PutBootcamp_UpdatesBootcamp_WhenDataIsValid

--- Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/UpdateBootcampControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/UpdateBootcampControllerTests.cs
@@ -1,4 +1,10 @@
+using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Bogus;
+using DiplomaMakerApi.Dtos;
+using DiplomaMakerApi.Models;
+using FluentAssertions;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.BootcampsController
@@ -6,10 +12,33 @@ namespace DiplomaMakerApi.Tests.Integration.BootcampsController
     public class UpdateBootcampControllerTests : IClassFixture<DiplomaMakerApiFactory>
     {
         private readonly HttpClient _client;
+        private readonly Faker<BootcampRequestDto> _bootcampPutRequestGenerator = 
+            new Faker<BootcampRequestDto>()
+                .RuleFor(b => b.GraduationDate, _ => DateTime.UtcNow)
+                .RuleFor(b => b.TrackId, faker => faker.Random.Int(1, 3));
         public UpdateBootcampControllerTests(DiplomaMakerApiFactory apiFactory)
         {
             _client = apiFactory.CreateClient();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "test-token");
+        }
+
+        [Fact]
+        public async Task PutBootcamp_UpdatesBootcamp_WhenDataIsValid()
+        {
+            // Arrange
+            var bootcampSetupRequest = await _client.GetAsync("api/Bootcamps");
+            var bootcampSetupResponse = await bootcampSetupRequest.Content.ReadFromJsonAsync<List<BootcampResponseDto>>();
+            var putBootcampRequest = _bootcampPutRequestGenerator.Generate();
+
+            // Act
+            var response = await _client.PutAsJsonAsync($"api/Bootcamps/{bootcampSetupResponse![0].GuidId}", putBootcampRequest);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var bootcampResponse = await response.Content.ReadFromJsonAsync<BootcampResponseDto>();
+            bootcampResponse!.GuidId.Should().Be(bootcampSetupResponse![0].GuidId);
+            bootcampResponse!.GraduationDate.Date.Should().Be(putBootcampRequest.GraduationDate.Date);
+            bootcampResponse!.GraduationDate.Date.Should().NotBe(bootcampSetupResponse[0].GraduationDate.Date);
         }
     }
 }
