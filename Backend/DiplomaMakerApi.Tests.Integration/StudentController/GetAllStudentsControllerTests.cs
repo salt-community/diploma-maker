84a0a446b17e3a0ce8c34801df68363a3a737a6f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-18 10:34:50 +0200

    test: GetAllStudentsControllerTests -> GetTemplates_ReturnsUnathorized_WhenInvalidToken

--- Backend/DiplomaMakerApi.Tests.Integration/StudentController/GetAllStudentsControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/StudentController/GetAllStudentsControllerTests.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.Http.Headers;
+using FluentAssertions;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.StudentController
@@ -10,6 +12,19 @@ namespace DiplomaMakerApi.Tests.Integration.StudentController
         {
             _client = apiFactory.CreateClient();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "test-token");
+        }
+
+        [Fact]
+        public async Task GetTemplates_ReturnsUnathorized_WhenInvalidToken()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Authorization = null;
+
+            // Act
+            var response = await _client.GetAsync("api/Templates");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
     }
 }
