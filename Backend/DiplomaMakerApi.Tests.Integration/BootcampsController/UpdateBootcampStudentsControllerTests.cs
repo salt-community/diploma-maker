a64cdaf76da854899e6920f01c9b68013f43beba
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-18 15:52:16 +0200

    setup: UpdateBootcampStudentsControllerTests -> Fakers

--- Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/UpdateBootcampStudentsControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BootcampsController/UpdateBootcampStudentsControllerTests.cs
@@ -1,3 +1,6 @@
+using Bogus;
+using DiplomaMakerApi.Dtos;
+using DiplomaMakerApi.Models;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.BootcampsController
@@ -5,9 +8,26 @@ namespace DiplomaMakerApi.Tests.Integration.BootcampsController
     public class UpdateBootcampStudentsControllerTests : IClassFixture<DiplomaMakerApiFactory>
     {
         private readonly HttpClient _client;
+        private readonly Faker<BootcampRequestDto> _bootcampPutRequestGenerator;
+        private readonly Faker<StudentRequestDto> _studentRequestsGenerator;
+        private readonly Faker<BootcampRequestUpdateDto> _bootcampStudentRequestGenerator;
         public UpdateBootcampStudentsControllerTests(DiplomaMakerApiFactory apiFactory)
         {
             _client = apiFactory.CreateClient();
+
+            _bootcampPutRequestGenerator = new Faker<BootcampRequestDto>()
+                .RuleFor(b => b.GraduationDate, _ => DateTime.UtcNow)
+                .RuleFor(b => b.TrackId, faker => faker.Random.Int(1, 3));
+
+            _studentRequestsGenerator = new Faker<StudentRequestDto>()
+                .RuleFor(s => s.GuidId, faker => Guid.NewGuid())
+                .RuleFor(s => s.VerificationCode, faker => faker.Random.AlphaNumeric(6))
+                .RuleFor(s => s.Name, faker => faker.Name.FullName())
+                .RuleFor(s => s.Email, faker => faker.Internet.Email());
+
+            _bootcampStudentRequestGenerator = new Faker<BootcampRequestUpdateDto>()
+                .RuleFor(b => b.templateId, faker => 1)
+                .RuleFor(b => b.students, faker => _studentRequestsGenerator.GenerateBetween(10, 15));
         }
     }
 }
