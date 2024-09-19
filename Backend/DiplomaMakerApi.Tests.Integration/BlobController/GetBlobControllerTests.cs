f64a5d0af24cffd6de249a02b092ef6134719d05
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-19 10:55:37 +0200

    test: GetBlobControllerTests -> GetFontsHeadRequest_ReturnsCorrectHeader_WhenFontExists

--- Backend/DiplomaMakerApi.Tests.Integration/BlobController/GetBlobControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BlobController/GetBlobControllerTests.cs
@@ -52,5 +52,38 @@ namespace DiplomaMakerApi.Tests.Integration.BlobController
                 fontContent.Should().BeEquivalentTo(fileMockData);
             }
         }
+
+        [Fact]
+        public async Task GetFontsHeadRequest_ReturnsCorrectHeader_WhenFontExists()
+        {
+            // Arrange
+            var fileMockData = Encoding.UTF8.GetBytes("fontMockData");
+            var setupUserFontsFormData = new MultipartFormDataContent
+            {
+                { new StringContent("testFont"), "UserFontRequests[0].Name" },
+                { new StringContent("regular"), "UserFontRequests[0].FontType" },
+                { new StreamContent(new MemoryStream(fileMockData)), "UserFontRequests[0].File", "testFont.woff" },
+
+                { new StringContent("testFont"), "UserFontRequests[1].Name" },
+                { new StringContent("bold"), "UserFontRequests[1].FontType" },
+                { new StreamContent(new MemoryStream(fileMockData)), "UserFontRequests[1].File", "testFont.woff" },
+
+                { new StringContent("testFont"), "UserFontRequests[2].Name" },
+                { new StringContent("italic"), "UserFontRequests[2].FontType" },
+                { new StreamContent(new MemoryStream(fileMockData)), "UserFontRequests[2].File", "testFont.woff" }
+            };
+            var setupResponse = await _client.PostAsync("api/UserFonts", setupUserFontsFormData);
+            var setupUserFontsResponse = await setupResponse.Content.ReadFromJsonAsync<List<UserFontResponseDto>>();
+
+            foreach (var fontType in new[] { "regular", "bold", "italic" })
+            {
+                // Act
+                var response = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Head, $"api/Blob/UserFonts/{setupUserFontsResponse![0].Name}?fontType={fontType}"));
+
+                // Assert
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+                response.Content.Headers.ContentType!.MediaType.Should().Be("font/woff");
+            }
+        }
     }
 }
