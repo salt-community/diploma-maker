bb7b4aee1b7617c3b7b342b8f7c366354c9145c0
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-19 21:35:24 +0200

    test: GetAllHistorySnapshotsControllerTests -> GetHistory_ReturnsAllHistory_WhenHistoryExists

--- Backend/DiplomaMakerApi.Tests.Integration/HistorySnapshotsController/GetAllHistorySnapshotsControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/HistorySnapshotsController/GetAllHistorySnapshotsControllerTests.cs
@@ -1,4 +1,10 @@
+using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Bogus;
+using DiplomaMakerApi.Dtos;
+using DiplomaMakerApi.Models;
+using FluentAssertions;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.HistorySnapshotsController
@@ -6,10 +12,41 @@ namespace DiplomaMakerApi.Tests.Integration.HistorySnapshotsController
     public class GetAllHistorySnapshotsControllerTests : IClassFixture<DiplomaMakerApiFactory>
     {
         private readonly HttpClient _client;
+        private readonly Faker<StudentRequestDto> _studentRequestsGenerator;
+        private readonly Faker<BootcampRequestUpdateDto> _bootcampStudentRequestGenerator;
         public GetAllHistorySnapshotsControllerTests(DiplomaMakerApiFactory apiFactory)
         {
             _client = apiFactory.CreateClient();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "test-token");
+
+            _studentRequestsGenerator = new Faker<StudentRequestDto>()
+                .RuleFor(s => s.GuidId, faker => Guid.NewGuid())
+                .RuleFor(s => s.VerificationCode, faker => faker.Random.AlphaNumeric(6))
+                .RuleFor(s => s.Name, faker => faker.Name.FullName())
+                .RuleFor(s => s.Email, faker => faker.Internet.Email());
+
+            _bootcampStudentRequestGenerator = new Faker<BootcampRequestUpdateDto>()
+                .RuleFor(b => b.templateId, faker => 1)
+                .RuleFor(b => b.students, faker => _studentRequestsGenerator.GenerateBetween(25, 30));
+        }
+
+        [Fact]
+        public async Task GetHistory_ReturnsAllHistory_WhenHistoryExists()
+        {
+            // Arrange
+            var bootcampSetupRequest = await _client.GetAsync("api/Bootcamps");
+            var bootcampSetupResponse = await bootcampSetupRequest.Content.ReadFromJsonAsync<List<BootcampResponseDto>>();
+            var updatedStudentsRequest = _bootcampStudentRequestGenerator.Generate(); 
+            await _client.PutAsJsonAsync($"api/Bootcamps/dynamicfields/{bootcampSetupResponse![0].GuidId}", updatedStudentsRequest);
+
+            // Act
+            var response = await _client.GetAsync("api/HistorySnapshots");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var historySnapshotsResponse = await response.Content.ReadFromJsonAsync<List<DiplomaSnapshotResponseDto>>();
+            historySnapshotsResponse!.Count.Should().Be(updatedStudentsRequest.students.Count);
+            historySnapshotsResponse[0].BasePdf.Should().Be($"Blob/DiplomaPdfs/Default.v1.pdf");
         }
     }
 }
