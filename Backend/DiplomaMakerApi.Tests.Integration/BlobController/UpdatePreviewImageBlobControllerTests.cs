c793fe090a5432c096e28ba7ded2010fc7373a2c
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-19 13:21:03 +0200

    test: UpdatePreviewImageBlobControllerTests -> UpdateStudentsPreviewImages_ReturnsBadRequest_WhenDataIsInvalid

--- Backend/DiplomaMakerApi.Tests.Integration/BlobController/UpdatePreviewImageBlobControllerTests.cs
+++ Backend/DiplomaMakerApi.Tests.Integration/BlobController/UpdatePreviewImageBlobControllerTests.cs
@@ -2,8 +2,10 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using DiplomaMakerApi.Dtos;
+using DiplomaMakerApi.MIddleware;
 using DiplomaMakerApi.Models;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace DiplomaMakerApi.Tests.Integration.BlobController
@@ -60,7 +62,28 @@ namespace DiplomaMakerApi.Tests.Integration.BlobController
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
-        
+
+        [Fact]
+        public async Task UpdateStudentsPreviewImages_ReturnsBadRequest_WhenDataIsInvalid()
+        {
+            // Arrange
+            var bootcampSetup = await _client.GetAsync("api/Bootcamps");
+            var bootcampSetupResponse = await bootcampSetup.Content.ReadFromJsonAsync<List<BootcampResponseDto>>();
+            var studentSetup = bootcampSetupResponse![0].Students.First();
+            var previewImageRequest = new MultipartFormDataContent
+            {
+                { new StringContent(studentSetup.GuidId.ToString()), "StudentGuidId" },
+                { new StringContent("badData"), "Image" }
+            };
+
+            // Act
+            var response = await _client.PutAsync("api/Blob/UpdateStudentsPreviewImage", previewImageRequest);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var errorResponse = await response.Content.ReadFromJsonAsync<CustomErrorResponse>();
+            errorResponse!.Message.Should().Be("The provided image is not a valid Base64 PDF string.");
+        }
 
         [Fact]
         public async Task UpdateBundledStudentsPreviewImages_UpdatesStudentPreviewImages_WhenDataIsValid()
