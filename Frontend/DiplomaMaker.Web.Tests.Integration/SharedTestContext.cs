e023938ff4207869b2218f371e126553c4806c85
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-23 10:28:31 +0200

    add: SharedTestContext -> playwright instance

--- Frontend/DiplomaMaker.Web.Tests.Integration/SharedTestContext.cs
+++ Frontend/DiplomaMaker.Web.Tests.Integration/SharedTestContext.cs
@@ -2,6 +2,7 @@
 using Ductus.FluentDocker.Builders;
 using Ductus.FluentDocker.Model.Common;
 using Ductus.FluentDocker.Services;
+using Microsoft.Playwright;
 
 namespace DiplomaMaker.Web.Tests.Integration;
 
@@ -17,13 +18,22 @@ public class SharedTestContext : IAsyncLifetime
         .RemoveOrphans()
         .WaitForHttp("diploma-maker-test-backend", "http://localhost:5000")
         .Build();
-
+    private IPlaywright? _playwright;
+    public IBrowser? Browser {get; private set;}
     public async Task InitializeAsync()
     {
         _dockerService.Start();
+        _playwright = await Playwright.CreateAsync();
+        Browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 1000
+        });
     }
     public async Task DisposeAsync()
     {
         _dockerService.Dispose();
+        await Browser!.DisposeAsync();
+        _playwright!.Dispose();
     }
 }
