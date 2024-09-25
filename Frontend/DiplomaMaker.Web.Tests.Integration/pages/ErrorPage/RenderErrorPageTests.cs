57ad3af714cb7790a4f8afd8161b62a56472ce11
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-25 16:01:52 +0200

    test: ErrorPage_ShouldRenderSuccessfully_WhenNavigating

---
+++ Frontend/DiplomaMaker.Web.Tests.Integration/pages/ErrorPage/RenderErrorPageTests.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+using Microsoft.Playwright;
+using Xunit;
+
+namespace DiplomaMaker.Web.Tests.Integration.pages.ErrorPage
+{
+    [Collection("Tests DiplomaMaker.Web")]
+    public class RenderErrorPageTests
+    {
+        private readonly SharedTestContext _testContext;
+        private readonly IPage _page;
+
+        public RenderErrorPageTests(SharedTestContext testContext)
+        {
+            _testContext = testContext;
+
+            _page = _testContext.Browser!.NewPageAsync(new BrowserNewPageOptions
+            {
+                BaseURL = SharedTestContext.AppUrl
+            }).GetAwaiter().GetResult();
+            
+            _page.GotoAsync("/sign-in").GetAwaiter().GetResult();
+            _page.FillAsync("input:nth-of-type(1)", _testContext.ClerkLoginUser!).GetAwaiter().GetResult();
+            _page.ClickAsync("button.cl-formButtonPrimary").GetAwaiter().GetResult();
+            _page.ClickAsync("button.cl-alternativeMethodsBlockButton").GetAwaiter().GetResult();
+            _page.FillAsync("input[name='password']", _testContext.ClerkLoginPassword!).GetAwaiter().GetResult();
+            _page.ClickAsync("span.cl-internal-2iusy0").GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public async Task ErrorPage_ShouldRenderSuccessfully_WhenNavigating()
+        {
+            // Arrange
+            var page = await _testContext.Browser!.NewPageAsync(new BrowserNewPageOptions{
+                BaseURL = SharedTestContext.AppUrl
+            });
+            await page.GotoAsync($"/{Guid.NewGuid()}");
+
+            // Act
+            var content = await page.TextContentAsync("body");
+
+            // Assert
+            content.Should().Contain("404");
+        }
+    }
+}
