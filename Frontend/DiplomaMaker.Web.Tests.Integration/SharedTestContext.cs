ec8705f2238350e20a84bb35cd2ab9b339d82712
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-25 16:24:55 +0200

    cleanup: SharedTestContext -> Uncessesary folder copying

--- Frontend/DiplomaMaker.Web.Tests.Integration/SharedTestContext.cs
+++ Frontend/DiplomaMaker.Web.Tests.Integration/SharedTestContext.cs
@@ -26,7 +26,6 @@ public class SharedTestContext : IAsyncLifetime
     public string? ClerkLoginPassword { get; private set; }
     public async Task InitializeAsync()
     {
-        cloneDefaultFiles();
         LoadConfiguration();
         _dockerService.Start();
         _playwright = await Playwright.CreateAsync();
@@ -41,7 +40,6 @@ public class SharedTestContext : IAsyncLifetime
         _dockerService.Dispose();
         await Browser!.DisposeAsync();
         _playwright!.Dispose();
-        cleanupTestFiles();
     }
 
     private void LoadConfiguration()
@@ -54,59 +52,5 @@ public class SharedTestContext : IAsyncLifetime
 
         ClerkLoginUser = configuration["clerk:loginuser"] ?? throw new ArgumentNullException("clerk:loginuser", "Clerk login user is not configured.");
         ClerkLoginPassword = configuration["clerk:loginpassword"] ?? throw new ArgumentNullException("clerk:loginpassword", "Clerk login password is not configured.");
-    }
-    private void cloneDefaultFiles()
-    {
-        var testProjectBinRoot = Directory.GetCurrentDirectory();
-        var solutionRoot = Path.GetFullPath(Path.Combine(testProjectBinRoot, "..", "..", "..", ".."));
-        var apiProjectRoot = Path.Combine(solutionRoot, "..", "Backend", "DiplomaMakerApi");
-        var basePdfTemplateFile = Path.Combine(apiProjectRoot, "Blob", "DiplomaPdfs", "Default.pdf");
-
-        var imagePreviewLQIPDirectory = Path.Combine(testProjectBinRoot, "Blob", "ImagePreviewLQIP");
-        var imagePreviewDirectory = Path.Combine(testProjectBinRoot, "Blob", "ImagePreview");
-        var userFontsDirectory = Path.Combine(testProjectBinRoot, "Blob", "UserFonts");
-
-        var destinationDirectory = Path.Combine(testProjectBinRoot, "Blob", "DiplomaPdfs");
-
-        if (!Directory.Exists(imagePreviewLQIPDirectory))
-        {
-            Directory.CreateDirectory(imagePreviewLQIPDirectory);
-        }
-
-        if (!Directory.Exists(imagePreviewDirectory))
-        {
-            Directory.CreateDirectory(imagePreviewDirectory);
-        }
-
-        if (!Directory.Exists(userFontsDirectory))
-        {
-            Directory.CreateDirectory(userFontsDirectory);
-        }
-
-        if (!Directory.Exists(destinationDirectory))
-        {
-            Directory.CreateDirectory(destinationDirectory);
-        }
-
-        var destinationFilePath = Path.Combine(destinationDirectory, "Default.pdf");
-
-        if (File.Exists(basePdfTemplateFile))
-        {
-            File.Copy(basePdfTemplateFile, destinationFilePath, overwrite: true);
-        }
-        else
-        {
-            throw new FileNotFoundException($"Basetemplatepdf not found (Blob/DiplomaPdfs/Default.pdf).");
-        }
-    }
-    private void cleanupTestFiles()
-    {
-        var testProjectBinRoot = Directory.GetCurrentDirectory();
-        var blobDirectoryPath = Path.Combine(testProjectBinRoot, "Blob");
-
-        if (Directory.Exists(blobDirectoryPath))
-        {
-            Directory.Delete(blobDirectoryPath, true);
-        }
     }
 }
