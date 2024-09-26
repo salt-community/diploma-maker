9d987ca60a2553da1ee48b0974e5f8f4f0818088
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-26 15:25:46 +0200

    fix: entrypoint.sh not recognized step1

--- Backend/DiplomaMakerApi/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Read secrets from files and export them as environment variables
-export Clerk__SecretKey=$(cat /run/secrets/clerk_secretkey)
-export Clerk__AuthorizedParty=$(cat /run/secrets/clerk_authorizedparty)
-export Clerk__Authority=$(cat /run/secrets/clerk_authority)
-
-# Now, start the dotnet application
-dotnet DiplomaMakerApi.dll
