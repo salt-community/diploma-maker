23ab324d101a8ee28ece6f3cbc69d58d69a23d2b
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-09-26 19:34:05 +0200

    test: GenerateDiplomasFlowTests -> NewBootcamp_ShouldAppearInBootcampTable_AfterAddingBootcamp

--- Frontend/DiplomaMaker.Web.Tests.E2E/actions/GenerateDiplomasFlowTests.cs
+++ Frontend/DiplomaMaker.Web.Tests.E2E/actions/GenerateDiplomasFlowTests.cs
@@ -32,6 +32,41 @@ namespace DiplomaMaker.Web.Tests.E2E.actions
         }
 
         [Fact]
+        public async Task NewBootcamp_ShouldAppearInBootcampTable_AfterAddingBootcamp()
+        {
+            // Arrange
+            await _page!.GotoAsync("/bootcamp-management");
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            // Act
+            var currentDate = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            await _page.FillAsync("div.modal-main-footer input.date-input", currentDate);
+            await _page.SelectOptionAsync("div.modal-main-footer select.normal", "2");
+            await _page.ClickAsync("div.modal-main-footer button.add-button--simple");
+
+            bool isDateFound = false;
+            int maxAttempts = 10;
+            int attempts = 0;
+
+            while (!isDateFound && attempts < maxAttempts)
+            {
+                var content = await _page.ContentAsync();
+                if (content.Contains(currentDate))
+                {
+                    isDateFound = true;
+                }
+                else
+                {
+                    await Task.Delay(1000);
+                    attempts++;
+                }
+            }
+
+            // Assert
+            isDateFound.Should().BeTrue($"The date {currentDate} should appear on the page.");           
+        }
+
+        [Fact]
         public async Task EnteredStudentName_ShouldAppearInTagList_AfterSubmission()
         {
             // Arrange
@@ -54,6 +89,13 @@ namespace DiplomaMaker.Web.Tests.E2E.actions
             // Arrange
             await _page!.GotoAsync("/pdf-creator");
             await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            var inputBox = await _page.QuerySelectorAsync("input.taginputbox");
+
+            await inputBox!.FillAsync("Bob Ryder");
+            await inputBox.PressAsync("Enter");
+
+            var content = await _page.ContentAsync();
+            content.Should().Contain("Bob Ryder", "Bob Ryder should be in the nametaginput.");
 
             // Act
             await _page!.ClickAsync(".diploma-making-form__submit-button");
