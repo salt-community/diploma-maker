402c17306d69a71b4d3da65080693d53faca7695
Oscar Hultén <oshulten@gmail.com>
2024-11-05 17:42:41 +0100

    refactor: clean ClerkService

--- Backend/DiplomaMakerApi/Services/ClerkService.cs
+++ Backend/DiplomaMakerApi/Services/ClerkService.cs
@@ -1,30 +1,28 @@
 using System.Text.Json;
 using System.Net.Http.Headers;
+
 using DiplomaMakerApi.Models.Clerk;
 
-namespace DiplomaMakerApi.Services
+namespace DiplomaMakerApi.Services;
+
+public class ClerkService(HttpClient _httpClient)
 {
-    public class ClerkService(HttpClient _httpClient)
+    public async Task<string> GetGoogleOAuthTokenAsync(string userId, string key)
     {
-        public async Task<string> GetGoogleOAuthTokenAsync(string userId, string key)
-        {
-            var url = $"https://api.clerk.com/v1/users/{userId}/oauth_access_tokens/oauth_google";
+        //TODO: Put url in app settings
+        var url = $"https://api.clerk.com/v1/users/{userId}/oauth_access_tokens/oauth_google";
 
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
 
-            var response = await _httpClient.GetAsync(url);
+        var response = await _httpClient.GetAsync(url);
 
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Failed to retrieve Google OAuth token from Clerk.");
-            }
+        if (!response.IsSuccessStatusCode)
+            throw new Exception("Failed to retrieve Google OAuth token from Clerk.");
 
-            var responseData = await response.Content.ReadAsStringAsync();
-            var serializedData = JsonSerializer.Deserialize<List<ClerkOauthGoogleResponse>>(responseData);
+        var responseData = await response.Content.ReadAsStringAsync();
+        var serializedData = JsonSerializer.Deserialize<List<ClerkOauthGoogleResponse>>(responseData);
 
-            return serializedData![0].Token ?? throw new Exception("Token not found in response.");
-        }
-
-        
+        return serializedData![0].Token
+            ?? throw new Exception("Token not found in response.");
     }
 }
