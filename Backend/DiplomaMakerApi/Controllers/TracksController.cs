1a516cae5ab9348b25a5c15898d563a54b13576f
Oscar Hultén <oshulten@gmail.com>
2024-11-05 17:16:27 +0100

    refactor: TracksController

--- Backend/DiplomaMakerApi/Controllers/TracksController.cs
+++ Backend/DiplomaMakerApi/Controllers/TracksController.cs
@@ -1,30 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
+
 using DiplomaMakerApi.Dtos;
 using DiplomaMakerApi.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DiplomaMakerApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 // [Authorize]
-public class TracksController : Controller
+public class TracksController(TrackService _trackservice, IMapper _mapper) : Controller
 {
-    private readonly TrackService _trackservice;
-    private readonly IMapper _mapper;
-
-    public TracksController(TrackService trackservice, IMapper mapper)
+    [HttpGet("GetTracks")]
+    public async Task<ActionResult<List<TracksResponseDto>>> GetTracks()
     {
-        _trackservice = trackservice;
-        _mapper = mapper;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<List<TracksResponseDto>>> GetTracks(){
-      var tracks = await _trackservice.GetAllTracks();
+        var tracks = await _trackservice.GetAllTracks();
         return _mapper.Map<List<TracksResponseDto>>(tracks);
-
-    } 
+    }
 
 }
