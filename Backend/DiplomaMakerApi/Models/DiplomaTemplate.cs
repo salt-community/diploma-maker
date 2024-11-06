53a4ddcf4e55313d9606d429a707f42bab5cfe45
Oscar Hultén <oshulten@gmail.com>
2024-11-06 16:16:13 +0100

    feat: move default stying to SeedData

--- Backend/DiplomaMakerApi/Models/DiplomaTemplate.cs
+++ Backend/DiplomaMakerApi/Models/DiplomaTemplate.cs
@@ -6,13 +6,13 @@ public class DiplomaTemplate
     public int Id { get; set; }
     public required string Name { get; set; }
     public string Footer { get; set; } = "has successfully completed\nthe {classname} Bootcamp of \n {datebootcamp} at School of Applied Technology.";
-    public TemplateStyle? FooterStyling { get; set; } = TemplateStyle.DefaultFooter;
+    public TemplateStyle? FooterStyling { get; set; }
     public string Intro { get; set; } = "This certifies that\n";
-    public TemplateStyle? IntroStyling { get; set; } = TemplateStyle.DefaultIntro;
+    public TemplateStyle? IntroStyling { get; set; }
     public string Main { get; set; } = "{studentname}";
-    public TemplateStyle? MainStyling { get; set; } = TemplateStyle.DefaultMain;
+    public TemplateStyle? MainStyling { get; set; }
     public string Link { get; set; } = "{id}";
-    public TemplateStyle? LinkStyling { get; set; } = TemplateStyle.DefaultLink;
+    public TemplateStyle? LinkStyling { get; set; }
     public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
     public DateTime? PdfBackgroundLastUpdated { get; set; } = DateTime.UtcNow;
 }
