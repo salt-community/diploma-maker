d024feadffeb27a2798bed26d7880c3353b19826
Oscar Hultén <oshulten@gmail.com>
2024-11-06 16:08:50 +0100

    feat: add default values for DiplomaTemplate

--- Backend/DiplomaMakerApi/Models/DiplomaTemplate.cs
+++ Backend/DiplomaMakerApi/Models/DiplomaTemplate.cs
@@ -6,13 +6,13 @@ public class DiplomaTemplate
     public int Id { get; set; }
     public required string Name { get; set; }
     public string Footer { get; set; } = "has successfully completed\nthe {classname} Bootcamp of \n {datebootcamp} at School of Applied Technology.";
-    public TemplateStyle? FooterStyling { get; set; }
+    public TemplateStyle? FooterStyling { get; set; } = TemplateStyle.DefaultFooter;
     public string Intro { get; set; } = "This certifies that\n";
-    public TemplateStyle? IntroStyling { get; set; }
+    public TemplateStyle? IntroStyling { get; set; } = TemplateStyle.DefaultIntro;
     public string Main { get; set; } = "{studentname}";
-    public TemplateStyle? MainStyling { get; set; }
+    public TemplateStyle? MainStyling { get; set; } = TemplateStyle.DefaultMain;
     public string Link { get; set; } = "{id}";
-    public TemplateStyle? LinkStyling { get; set; }
+    public TemplateStyle? LinkStyling { get; set; } = TemplateStyle.DefaultLink;
     public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
     public DateTime? PdfBackgroundLastUpdated { get; set; } = DateTime.UtcNow;
 }
