2ce1a1e86962e15f7397280765770dde0f580207
Oscar Hultén <oshulten@gmail.com>
2024-11-07 20:02:27 +0100

    feat: consolidated micro components in Designer

--- Frontend/DiplomaMaker.Web.2/src/components/PdfMe/Designer.tsx
+++ Frontend/DiplomaMaker.Web.2/src/components/PdfMe/Designer.tsx
@@ -2,11 +2,62 @@ import { Template } from "@pdfme/common";
 import { useRef } from "react";
 import { useDesigner } from "../../hooks/useDesigner";
 
-export default function Designer(template?: Template) {
+interface Props {
+    template?: Template
+}
+
+export default function Designer({ template }: Props) {
     const containerRef = useRef<HTMLDivElement | null>(null);
-    useDesigner(containerRef, template);
+
+    const {
+        onDownloadTemplate,
+        onResetTemplate,
+        handleLoadTemplate,
+        onChangeBasePdf } = useDesigner(containerRef, template);
+
+    const DownloadTemplateButton = (
+        <button
+            className='btn'
+            onClick={onDownloadTemplate}>
+            Download Template
+        </button>
+    );
+
+    const ResetTemplateButton = (
+        <button
+            className=''
+            onClick={onResetTemplate}>
+            Reset Template
+        </button>
+    );
+
+    const LoadTemplateInput = (
+        <input
+            className=''
+            type="file"
+            accept="application/json"
+            onChange={handleLoadTemplate} />
+    );
+
+    const ChangeBasePdfInput = (
+        <input
+            className=''
+            type="file"
+            accept="application/pdf"
+            onChange={onChangeBasePdf} />
+    );
+
+    const DesignerContainer = (
+        <div ref={containerRef}></div>
+    );
 
     return (
-        <div ref={containerRef}></div>
+        <>
+            {DownloadTemplateButton}
+            {ResetTemplateButton}
+            {LoadTemplateInput}
+            {ChangeBasePdfInput}
+            {DesignerContainer}
+        </>
     );
 }
