5eb9d1dc0245074f24474e94f0eb6851fc20dfef
Oscar Hultén <oshulten@gmail.com>
2024-11-08 17:57:22 +0100

    feat: extend template substitution types

--- Frontend/DiplomaMaker.Web.2/src/types/types.ts
+++ Frontend/DiplomaMaker.Web.2/src/types/types.ts
@@ -1,12 +1,14 @@
 export type TemplateSubstitutions = {
     text: Record<string, string>,
     images: Record<string, string>,
+    qrCodes: Record<string, string>,
     basePdf: string
 }
 
 export type TemplateImageSubstitutions = {
     basePdf: string,
     images: string[]
+    qrCodes: string[]
 };
 
 export type TemplateInputs = Record<string, unknown>[];
