7ab970b035ef9729d8b382cb9f2235fe6f812c46
Oscar Hultén <oshulten@gmail.com>
2024-11-09 11:44:12 +0100

    feat: add MockerService

--- Backend/DiplomaMakerApi.2/Models/Models.cs
+++ Backend/DiplomaMakerApi.2/Models/Models.cs
@@ -1,4 +1,40 @@
+/*
+    Models
+
+    The models used in the backend to form the database.
+    Each derives from BaseEntity, which provides each with
+    and Id, a Guid and the generic Patch method.
+
+    Each property is nullable to generalize the Patch method,
+    allowing an entity to be updated by the same entity type,
+    skipping null values in the patch entity.
+
+    The entities are sorted from least coupled to most.
+*/
+
 namespace DiplomaMakerApi._2.Models;
+
+public class Track() : BaseEntity<Track>()
+{
+    public string? Name { get; set; } = string.Empty;
+};
+
+public class Student() : BaseEntity<Student>()
+{
+    public string? Name { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
+}
+
+public class StringFile() : BaseEntity<StringFile>()
+{
+    public string? FileType { get; set; } = string.Empty;
+    public string? Content { get; set; } = string.Empty;
+}
+
+public class Template() : BaseEntity<Template>()
+{
+    public Guid? BasePdfGuid { get; set; } = null;
+}
 
 public class Bootcamp() : BaseEntity<Bootcamp>()
 {
@@ -12,25 +48,3 @@ public class Diploma() : BaseEntity<Diploma>()
     public Bootcamp? Bootcamp { get; set; } = new();
     public Template? Template { get; set; } = new();
 }
-
-public class StringFile() : BaseEntity<StringFile>()
-{
-    public string? FileType { get; set; } = string.Empty;
-    public string? Content { get; set; } = string.Empty;
-}
-
-public class Student() : BaseEntity<Student>()
-{
-    public string? Name { get; set; } = string.Empty;
-    public string? Email { get; set; } = string.Empty;
-}
-
-public class Template() : BaseEntity<Template>()
-{
-    public Guid? BasePdfGuid { get; set; } = null;
-}
-
-public class Track() : BaseEntity<Track>()
-{
-    public string? Name { get; set; } = string.Empty;
-};
---
+++ Backend/DiplomaMakerApi.2/Services/MockerService.cs
@@ -0,0 +1,73 @@
+/*
+    MockerService
+
+    Provides methods to mock model entities, used for database seeding.
+*/
+
+using Bogus;
+using DiplomaMakerApi._2.Models;
+
+namespace DiplomaMakerApi._2.Database;
+
+public static class MockerService
+{
+    private static T RandomElement<T>(List<T> set) =>
+        set[Random.Shared.Next(set.Count)];
+
+    private static List<T> RandomSubset<T>(List<T> set)
+    {
+        int count = Random.Shared.Next(set.Count);
+
+        List<T> subset = [];
+        while (subset.Count < count)
+        {
+            var randomElement = RandomElement(set);
+            subset.Add(randomElement);
+            set.Remove(randomElement);
+        }
+
+        return subset;
+    }
+
+    public static IEnumerable<Track> MockTracks(int count) =>
+        new Faker<Track>()
+        .RuleFor(track => track.Name, f => f.Lorem.Words(1)[0])
+            .Generate(count);
+
+    public static IEnumerable<Student> MockStudents(int count) =>
+        new Faker<Student>()
+            .RuleFor(student => student.Name, f => f.Person.Name)
+            .RuleFor(student => student.Email, f => f.Person.Email)
+            .Generate(count);
+
+    public static IEnumerable<Template> MockTemplates(List<Guid> basePdfGuids, int count) =>
+        new Faker<Template>()
+            .RuleFor(template => template.BasePdfGuid, f => RandomElement(basePdfGuids))
+            .Generate(count);
+
+    public static IEnumerable<StringFile> MockStringFiles(List<string> fileTypes, int count) =>
+        new Faker<StringFile>()
+            .RuleFor(stringFile => stringFile.FileType, f => RandomElement(fileTypes))
+            .RuleFor(stringFile => stringFile.Content, f => f.Image.Image())
+            .Generate(count);
+
+    public static IEnumerable<Bootcamp> MockBootcamp(
+        List<Student> students,
+        List<Track> tracks,
+        int count) =>
+        new Faker<Bootcamp>()
+            .RuleFor(bootcamp => bootcamp.Students, f => RandomSubset(students))
+            .RuleFor(bootcamp => bootcamp.Track, f => RandomElement(tracks))
+            .Generate(count);
+
+    public static IEnumerable<Diploma> MockDiploma(
+        List<Student> students,
+        List<Bootcamp> bootcamps,
+        List<Template> templates,
+        int count) =>
+        new Faker<Diploma>()
+            .RuleFor(diploma => diploma.Student, f => RandomElement(students))
+            .RuleFor(diploma => diploma.Bootcamp, f => RandomElement(bootcamps))
+            .RuleFor(diploma => diploma.Template, f => RandomElement(templates))
+            .Generate(count);
+}
