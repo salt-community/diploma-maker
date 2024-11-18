2f20ad2fbd6298c9084daa089230188adfaa00e9
Abed Fetrat <abedfetrat@gmail.com>
2024-11-18 16:04:57 +0100

    chore: configure theme

--- Frontend/DiplomaMaker.Web/tailwind.config.js
+++ Frontend/DiplomaMaker.Web/tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
 
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      display: ["Poppins", ...defaultTheme.fontFamily.sans],
+      body: ["Manrope", ...defaultTheme.fontFamily.sans],
     },
-    plugins: [daisyui],
+  },
+  daisyui: {
+    themes: [
+      {
+        salt: {
+          primary: "#FF7961",
+          secondary: "#042D45",
+          accent: "#ECF7FA",
+          neutral: "#ffffff",
+          "base-100": "#f9f9f9",
+          "base-content": "#042D45",
+          info: "#FAECF8",
+          success: "#E1FFED",
+          warning: "#fde68a",
+          error: "#fca5a5",
+        },
+      },
+    ],
+  },
+  plugins: [daisyui, typography],
 };
