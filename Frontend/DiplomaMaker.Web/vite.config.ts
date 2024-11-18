be04efdf7da2f8dab0a952ce720208795fd5795d
Abed Fetrat <abedfetrat@gmail.com>
2024-11-18 17:40:52 +0100

    chore: add hugeicons and svgr for importing svgs as components

--- Frontend/DiplomaMaker.Web/vite.config.ts
+++ Frontend/DiplomaMaker.Web/vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tsconfigPaths(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
 });
