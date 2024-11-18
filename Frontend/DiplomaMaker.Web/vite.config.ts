ea4c87f332075f4713b533bacf39492a69e0c7fb
Abed Fetrat <abedfetrat@gmail.com>
2024-11-18 16:06:10 +0100

    chore: add path mapping support

--- Frontend/DiplomaMaker.Web/vite.config.ts
+++ Frontend/DiplomaMaker.Web/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite()
-  ],
-})
+  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
+});
