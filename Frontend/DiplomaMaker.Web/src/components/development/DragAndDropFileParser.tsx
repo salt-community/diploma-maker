593b57cb14c9a296b28b5b68a1ac43e6e10de51c
Oscar Hultén <oshulten@gmail.com>
2024-11-18 14:12:46 +0100

    feat: can parse json and csv

---
+++ Frontend/DiplomaMaker.Web/src/components/development/DragAndDropFileParser.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { FileService } from "../../services";
+
+const fileTypes = ["JSON"];
+
+export default function DragAndDropFileParser() {
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleChange = async (file: any) => {
+        try {
+            const bootcamp = await FileService.parseJsonFileIntoBootcamp(file);
+            console.log(bootcamp);
+        } catch (error) {
+            console.log((error as Error).message);
+        }
+    };
+
+    return (
+        <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+    );
+}
--- Frontend/DiplomaMaker.Web/src/routes/__root.tsx
+++ Frontend/DiplomaMaker.Web/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import DragAndDropFileParser from '../components/development/DragAndDropFileParser'
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <Outlet />
+            <DragAndDropFileParser />
             <TanStackRouterDevtools />
             <ReactQueryDevtools />
         </>
