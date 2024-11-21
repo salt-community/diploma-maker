116da54778bfe1294b2bb335b5559fea923b6d30
Oscar Hultén <oshulten@gmail.com>
2024-11-21 14:10:59 +0100

    feat: BootcampForm is laid out with table

--- Frontend/DiplomaMaker.Web/src/components/diploma-generator/BootcampForm.tsx
+++ Frontend/DiplomaMaker.Web/src/components/diploma-generator/BootcampForm.tsx
@@ -11,70 +11,112 @@ type Student = {
 }
 
 export default function BootcampForm({ bootcamp }: Props) {
-    console.log(bootcamp);
-    const studentsDefaultValues = bootcamp
-        ? bootcamp.students.map(student => ({
-            name: student.name,
-            email: student.email
-        } as Student))
-        : [];
-
-    console.log(studentsDefaultValues);
+    const formValues = bootcamp
+        ?? {
+            track: "Track",
+            graduationDate: new Date(Date.now()),
+            students: []
+        } as Bootcamp;
 
     const { register, control, handleSubmit } = useForm<Bootcamp>({
-        values: {
-            track: "CSharp",
-            students: studentsDefaultValues,
-            graduationDate: new Date(Date.now())
-            // students: [
-            //     {
-            //         name: "jfsklöa",
-            //         email: "jfkslöa"
-            //     },
-            //     {
-            //         name: "jfssegklöa",
-            //         email: "jfgeskslöa"
-            //     },
-            // ]
-        }
+        values: formValues
     });
     const { fields, append, remove } = useFieldArray<Bootcamp>({
         control,
         name: 'students',
     });
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data: Bootcamp) => console.log(data);
+
+    const header = (
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    );
+
+    const rows = bootcamp && bootcamp.students.map((student, i) => (
+        <tr key={student.email}>
+            <td>
+                <input
+                    {...register(`students.${i}.name`)}
+                    type="text"
+                    className="input input-bordered w-full max-w-xs"
+                />
+            </td>
+            <td>
+                <input
+                    {...register(`students.${i}.email`)}
+                    type="text"
+                    className="input input-bordered w-full max-w-xs"
+                />
+            </td>
+        </tr>
+    ));
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                className="input input-bordered w-full max-w-xs"
-                type="text" {...register('track')} />
-            {fields.map((field, index) => (
-                <div key={field.id}>
-                    <input
-                        {...register(`students.${index}.name`)}
-                        className="input input-bordered w-full max-w-xs"
-                        defaultValue={field.name}
-                    />
-                    <input
-                        {...register(`students.${index}.email`)}
-                        className="input input-bordered w-full max-w-xs"
-                        defaultValue={field.email}
-                    />
-                    <button type="button" onClick={() => remove(index)}>
-                        Remove
-                    </button>
-                </div>
-            ))}
+        <div className="overflow-x-auto">
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-            <button className="btn" type="button" onClick={() => append({ name: '', email: 0 })}>
-                Add Item
-            </button>
 
-            <button className="btn" type="submit">Submit</button>
-        </form>
+                <table className="table">
+
+                    <thead>
+                        {header}
+                    </thead>
+
+                    <tbody>
+                        {rows}
+                    </tbody>
+
+                </table>
+
+                {/* <button
+                    className="btn bg-warning hocus:bg-error"
+                    onClick={() => {
+                        setBootcamp({
+                            ...bootcamp,
+                            students: [...bootcamp.students, {
+                                name: "John Doe",
+                                email: "john.doe@appliedtechonology.se"
+                            }]
+                        })
+                    }}>
+                    Add Student
+                </button> */}
+            </form>
+        </div >
     );
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    //     <input
+    //         className="input input-bordered w-full max-w-xs"
+    //         type="text" {...register('track')} />
+
+    //     {fields.map((field, index) => (
+    //         <div key={field.id}>
+    //             <input
+    //                 {...register(`students.${index}.name`)}
+    //                 className="input input-bordered w-full max-w-xs"
+    //                 defaultValue={field.name}
+    //             />
+    //             <input
+    //                 {...register(`students.${index}.email`)}
+    //                 className="input input-bordered w-full max-w-xs"
+    //                 defaultValue={field.email}
+    //             />
+    //             <button type="button" onClick={() => remove(index)}>
+    //                 Remove
+    //             </button>
+    //         </div>
+    //     ))}
+
+    //     <button className="btn" type="button" onClick={() => append({ name: 'Student Name', email: "student.name@appliedtechnology.se" })}>
+    //         Add Item
+    //     </button>
+
+    //     <button className="btn" type="submit">Submit</button>
+    // </form>
+    // );
     // const [bootcamp, setBootcamp] = useCache<Bootcamp>(["Bootcamp"]);
 
     // const {
