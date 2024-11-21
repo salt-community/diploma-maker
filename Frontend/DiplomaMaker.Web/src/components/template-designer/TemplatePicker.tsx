9873be0c1a653daa1b9672fa13b565ec2d0f5332
Oscar Hultén <oshulten@gmail.com>
2024-11-21 07:12:05 +0100

    feat: add ManageTemplatesModal

--- Frontend/DiplomaMaker.Web/src/components/template-designer/TemplatePicker.tsx
+++ Frontend/DiplomaMaker.Web/src/components/template-designer/TemplatePicker.tsx
@@ -6,6 +6,7 @@ import { useModal } from "@/hooks/useModal";
 import NewTemplateModal from "./NewTemplateModal";
 import useCache from "@/hooks/useCache";
 import { selectedTemplateKey } from "./cacheKeys";
+import ManageTemplatesModal from "./ManageTemplatesModal";
 
 type TemplatePickerProps = {
   onTemplateSelect: () => void;
@@ -17,7 +18,8 @@ export default function TemplatePicker({
   onNewTemplate,
 }: TemplatePickerProps) {
   const { templatePeeks } = useTemplates();
-  const { openModal } = useModal(import.meta.env.VITE_NEW_TEMPLATE_MODAL_ID);
+  const { openModal: openNewTemplatesModal } = useModal(import.meta.env.VITE_NEW_TEMPLATE_MODAL_ID);
+  const { openModal: openManageTemplatesModal } = useModal(import.meta.env.VITE_MANAGE_TEMPLATES_MODAL_ID);
   const [selectedTemplate, setSelectedTemplate] = useCache<NamedEntity>(selectedTemplateKey);
 
   const handleSelectTemplate = (template: NamedEntity) => {
@@ -42,42 +44,47 @@ export default function TemplatePicker({
   };
 
   return (
-    <div className="dropdown dropdown-bottom">
-      <div
-        tabIndex={0}
-        role="button"
-        className="btn m-1 flex min-w-64 justify-start border-none bg-base-100 text-start"
-      >
-        <div className="flex-1">
-          <span className="font-medium">Template</span>
-          <p className="font-display text-base font-semibold">
-            {selectedTemplate ? selectedTemplate.name : "Select a template"}
-          </p>
+    <>
+      <div className="dropdown dropdown-bottom">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn m-1 flex min-w-64 justify-start border-none bg-base-100 text-start"
+        >
+          <div className="flex-1">
+            <span className="font-medium">Template</span>
+            <p className="font-display text-base font-semibold">
+              {selectedTemplate ? selectedTemplate.name : "Select a template"}
+            </p>
+          </div>
+          <ArrowDown01Icon size={18} />
         </div>
-        <ArrowDown01Icon size={18} />
+        <ul
+          tabIndex={0}
+          className="menu dropdown-content z-[1] w-full rounded-box bg-base-100 p-2 text-base shadow">
+          {renderSelectItems()}
+          <li className="pt-1">
+            <button
+              className="bg-primary text-primary-content hocus:bg-primary-focus"
+              onClick={() => openNewTemplatesModal()}>
+              <Add01Icon size={16} />
+              Create new Template
+            </button>
+          </li>
+          <li className="pt-1">
+            <button
+              className="bg-secondary text-secondary-content hocus:bg-secondary-focus"
+              onClick={() => openManageTemplatesModal()}>
+              <PencilEdit01Icon size={16} />
+              Manage Templates
+            </button>
+          </li>
+        </ul>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu dropdown-content z-[1] w-full rounded-box bg-base-100 p-2 text-base shadow">
-        {renderSelectItems()}
-        <li className="pt-1">
-          <button
-            className="bg-primary text-primary-content hocus:bg-primary-focus"
-            onClick={() => openModal()}>
-            <Add01Icon size={16} />
-            Create new Template
-          </button>
-        </li>
-        <li className="pt-1">
-          <Link
-            to="/"
-            className="bg-secondary text-secondary-content hocus:bg-secondary-focus">
-            <PencilEdit01Icon size={16} />
-            Manage Templates
-          </Link>
-        </li>
-      </ul>
+
+      {/* Modals */}
       <NewTemplateModal onCreateNewTemplate={onNewTemplate} />
-    </div>
+      <ManageTemplatesModal />
+    </>
   );
 }
