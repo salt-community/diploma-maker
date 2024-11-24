6aed1697dab65210530cc0f9fa6218dc009e8de0
Oscar Hultén <oshulten@gmail.com>
2024-11-24 15:03:49 +0100

    feat: simplify useHistoricDiploma by making it singular

--- Frontend/DiplomaMaker.Web/src/components/diploma-viewer/useHistoricDiploma.ts
+++ Frontend/DiplomaMaker.Web/src/components/diploma-viewer/useHistoricDiploma.ts
@@ -4,48 +4,21 @@
     Corresponds to the HistoricDiplomaController in backend.
 */
 
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { BackendService, BackendTypes } from "@/services";
-import { useEntity } from "@/hooks/useEntity";
 
-export function useHistoricDiploma() {
-    const client = useQueryClient();
-
-    const queryKey = ["HistoricDiploma"];
-
-    const { entities: diplomas } = useEntity<BackendTypes.DiplomaRecord>("DiplomaRecord", true);
+export function useHistoricDiploma(diplomaGuid: string) {
+    const queryKey = ["HistoricDiploma", diplomaGuid];
 
     const historicDiplomaQuery = useQuery({
         queryKey,
-        queryFn: () => [] as BackendTypes.HistoricDiploma[]
+        queryFn: async () => await BackendService.getHistoricDiplomaByGuid(diplomaGuid);
     });
 
-    const historicDiplomas = historicDiplomaQuery.data as BackendTypes.HistoricDiploma[];
-
-    const getHistoricDiplomaMutation = useMutation({
-        mutationFn: async (guid: string) => await BackendService.getHistoricDiplomaByGuid(guid) as BackendTypes.HistoricDiploma,
-        onSuccess: (response: BackendTypes.HistoricDiploma) => updateCacheWith(response),
-    });
-
-    function getHistoricDiploma(guid: string) {
-        console.log(historicDiplomas);
-        const existingDiploma = historicDiplomas.find(diploma => diploma.guid == guid);
-
-        if (existingDiploma) return existingDiploma;
-
-        getHistoricDiplomaMutation.mutate(guid);
-    }
-
-    function updateCacheWith(historicDiploma: BackendTypes.HistoricDiploma) {
-        client.setQueryData(
-            queryKey,
-            [...historicDiplomas.filter((existingEntity) => existingEntity.guid != historicDiploma.guid), historicDiploma]
-        );
-    };
+    const historicDiploma = historicDiplomaQuery.data as BackendTypes.HistoricDiploma | null;
 
     return {
-        diplomas,
-        getHistoricDiploma: (guid: string) => getHistoricDiploma(guid),
+        historicDiploma,
     }
 }
