d02e053df82644d8b20610e01b37a7d6123e71fa
Oscar Hultén <oshulten@gmail.com>
2024-11-24 13:23:14 +0100

    feat: extract out code from history page to DiplomaHistory

--- Frontend/DiplomaMaker.Web/src/routes/history.tsx
+++ Frontend/DiplomaMaker.Web/src/routes/history.tsx
@@ -4,60 +4,22 @@ import { StringService } from '@/services';
 import HistoricDiplomaViewer from '@/components/diploma-viewer/HistoricDiplomaViewer';
 import { Modal, PageLayout } from '@/components/layout';
 import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import DiplomaHistory from '@/components/DiplomaHistory';
 
 export const Route = createFileRoute('/history')({
   component: Page,
 })
 
 function Page() {
-  const { diplomas } = useHistoricDiploma();
-  const [diplomaGuid, setDiplomaGuid] = useCache<string>(["SelectedDiplomaGuid"]);
-  const { openModal: openHistoricDiplomaViewerModal } = useModal(import.meta.env.VITE_HISTORIC_DIPLOMA_VIEWER_MODAL_ID);
+  return (
+    <PageLayout>
+      <SignedIn>
+        <DiplomaHistory />
+      </SignedIn>
 
-  const headerTitles = ['Student Name', 'Student Email', 'Track', 'Graduation Date', ''];
-  const header = (
-    <thead>
-      <tr>{headerTitles.map(title => <th key={title}>{title}</th>)}</tr>
-    </thead>);
-
-  const rows = diplomas.map(diploma => (
-    <tr key={diploma.guid}>
-      <td>{diploma.studentName}</td>
-      <td>{diploma.studentEmail}</td>
-      <td>{diploma.track}</td>
-      <td>{StringService.formatDate_YYYY_mm_dd(diploma.graduationDate)}</td>
-      <td>
-        <button
-          className="btn"
-          onClick={() => {
-            setDiplomaGuid(diploma.guid!);
-            openHistoricDiplomaViewerModal();
-          }}>
-          Preview Diploma
-        </button>
-      </td>
-    </tr>
-  ));
-
-  return (<PageLayout>
-    <SignedIn>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {header}
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
-      </div>
-      <Modal id={import.meta.env.VITE_HISTORIC_DIPLOMA_VIEWER_MODAL_ID} title="Historic Diploma">
-        {diplomaGuid &&
-          <HistoricDiplomaViewer diplomaGuid={diplomaGuid} />
-        }
-      </Modal>
-    </SignedIn>
-
-    <SignedOut>
-      <Navigate to={"/"} />
-    </SignedOut>
-  </PageLayout>);
+      <SignedOut>
+        <Navigate to={"/"} />
+      </SignedOut>
+    </PageLayout>
+  );
 }
