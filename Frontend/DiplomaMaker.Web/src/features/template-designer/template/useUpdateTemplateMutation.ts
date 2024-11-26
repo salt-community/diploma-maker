70987d06940aae69b5a4a61ef88513c70c9ad9f1
Abed Fetrat <abedfetrat@gmail.com>
2024-11-26 12:41:07 +0100

    feat: add func to rename templates

--- Frontend/DiplomaMaker.Web/src/features/template-designer/template/useUpdateTemplateMutation.ts
+++ Frontend/DiplomaMaker.Web/src/features/template-designer/template/useUpdateTemplateMutation.ts
@@ -11,6 +11,17 @@ export default function useUpdateTemplateMutation() {
       BackendService.putEntity("Template", template),
     onSuccess: (template) => {
       queryClient.setQueryData(["templates", template.guid], template);
+
+      // Updates the template name in the templatePeeks list
+      queryClient.setQueryData(
+        ["templatePeeks"],
+        (old: BackendTypes.TemplatePeek[]) =>
+          old.map((peek) =>
+            peek.guid === template.guid
+              ? { ...peek, name: template.name }
+              : peek,
+          ),
+      );
     },
   });
 }
